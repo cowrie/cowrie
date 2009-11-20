2d363cee9acdce592355b5ae41e4044342b2dd0f
desaster <desaster@951d7100-d841-11de-b865-b3884708a8e2>
2009-11-20 15:49:23 +0000

    forgot to include dice.py in last update
    git-svn-id: https://kippo.googlecode.com/svn/trunk@35 951d7100-d841-11de-b865-b3884708a8e2

---
+++ commands/dice.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2009 Upi Tamminen <desaster@gmail.com>
+# See the COPYRIGHT file for more information
+
+# Random commands when running new executables
+
+from core.honeypot import HoneyPotCommand
+
+commands = {}
+clist = []
+
+class command_orly(HoneyPotCommand):
+    def start(self):
+        self.orly()
+
+    def orly(self):
+        self.writeln(' {o,o}')
+        self.writeln(' |)__)')
+        self.writeln(' -"-"-')
+        self.write('O RLY? ')
+
+    def lineReceived(self, data):
+        if data.strip().lower() in ('ya', 'yarly', 'ya rly', 'yes', 'y'):
+            self.writeln('  ___')
+            self.writeln(' {o,o}')
+            self.writeln(' (__(|')
+            self.writeln(' -"-"-')
+            self.writeln('NO WAI!')
+            self.exit()
+            return
+        self.orly()
+clist.append(command_orly)
