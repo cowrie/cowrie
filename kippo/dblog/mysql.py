10e8b2e3ec80cfe5c0a7bfa63a86c045b3b91af0
desaster <desaster@951d7100-d841-11de-b865-b3884708a8e2>
2010-08-14 12:36:20 +0000

    try not using reconnecting hack for adbapi to fix Dave_w's problem
    git-svn-id: https://kippo.googlecode.com/svn/trunk@165 951d7100-d841-11de-b865-b3884708a8e2

--- kippo/dblog/mysql.py
+++ kippo/dblog/mysql.py
@@ -32,7 +32,7 @@ class ReconnectingConnectionPool(adbapi.ConnectionPool):
 
 class DBLogger(dblog.DBLogger):
     def start(self, cfg):
-        self.db = ReconnectingConnectionPool('MySQLdb',
+        self.db = adbapi.ConnectionPool('MySQLdb',
             host = cfg.get('database', 'host'),
             db = cfg.get('database', 'database'),
             user = cfg.get('database', 'username'),
