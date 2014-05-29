91bf09b17b07a5a8ba7aefc095af4090cee63a94
g0tmi1k <have.you.g0tmi1k@gmail.com>
2014-05-29 14:57:31 +0100

    Shouldnt have to be in the root folder to start

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo -n "Starting kippo in background..."
+echo -e "[\e[01;32mi\e[00m] Starting kippo in the background...\n"
+cd $(dirname $0)
 twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid
