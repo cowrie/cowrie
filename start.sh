84793d368347f23df41d45e8d1a3637037b85652
g0tmi1k <have.you.g0tmi1k@gmail.com>
2014-06-10 08:09:44 +0100

    Making it match 'stop.sh' Related commit: 0c8da1e098f61eeb5ce5e0893bd2ffaf03f0e920

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-echo -e "[\e[01;32mi\e[00m] Starting kippo in the background...\n"
+echo "Starting kippo in the background...\n"
 cd $(dirname $0)
 twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid
+
