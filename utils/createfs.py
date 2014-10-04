9d5930e109e8ad69eabde1acb85e022ec12c9940
Michel Oosterhof <michel@oosterhof.net>
2014-10-04 19:22:27 +0000

    Fix timestamp issue when gen'ing fs (Issue #83)

--- utils/createfs.py
+++ utils/createfs.py
@@ -26,7 +26,7 @@ def recurse(root, tree, count = 0):
             continue
 
         entry = [name, T_FILE, s.st_uid, s.st_gid, s.st_size, s.st_mode, \
-            int(s.st_ctime), [], None, None]
+            int(s.st_mtime), [], None, None]
 
         if S_ISLNK(s[ST_MODE]):
             entry[A_TYPE] = T_LINK
