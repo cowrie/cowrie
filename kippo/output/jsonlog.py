90d2c656be9ba47cca8f70a1c44d122e05a269d9
Michel Oosterhof <michel@oosterhof.net>
2015-04-30 16:43:20 +0000

    fix pylint warnings

--- kippo/output/jsonlog.py
+++ kippo/output/jsonlog.py
@@ -26,7 +26,6 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 
-import abc
 import json
 import os
 
@@ -36,18 +35,21 @@ import kippo.core.output
 
 class Output(kippo.core.output.Output):
 
+    def __init__(self):
+        self.outfile = None
+
     def start(self, cfg):
         fn = cfg.get('output_jsonlog', 'logfile')
         dir = os.path.dirname(fn)
         base = os.path.basename(fn)
-        self.outfile = twisted.python.logfile.DailyLogFile( base, dir )
+        self.outfile = twisted.python.logfile.DailyLogFile(base, dir)
 
     def stop(self):
         self.outfile.close()
 
     def write(self, logentry):
-        json.dump( logentry,  self.outfile )
-        self.outfile.write( '\n' )
+        json.dump(logentry, self.outfile)
+        self.outfile.write('\n')
         self.outfile.flush()
 
 # vim: set sw=4 et:
