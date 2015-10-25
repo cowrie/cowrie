3056c83294932f5d6cb277bb627d2c9c7ab51516
Michel Oosterhof <michel@oosterhof.net>
2015-10-25 15:50:05 +0400

    more standard output for apt-get

--- cowrie/commands/apt.py
+++ cowrie/commands/apt.py
@@ -24,7 +24,11 @@ suppports only the 'install PACKAGE' command & 'moo'.
 Any installed packages, places a 'Segfault' at /usr/bin/PACKAGE.'''
 class command_aptget(HoneyPotCommand):
     def start(self):
-        if len(self.args) > 0 and self.args[0] == 'install':
+	if len(self.args) == 0:
+	    self.do_help()
+	elif len(self.args) > 0 and self.args[0] == '-v':
+	    self.do_version()
+        elif len(self.args) > 0 and self.args[0] == 'install':
             self.do_install()
         elif len(self.args) > 0 and self.args[0] == 'moo':
             self.do_moo()
@@ -37,6 +41,69 @@ class command_aptget(HoneyPotCommand):
             time = random.randint(time * 100, time2 * 100) / 100.0
         reactor.callLater(time, d.callback, None)
         return d
+
+    def do_version(self):
+	self.writeln("""apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06
+Supported modules:
+*Ver: Standard .deb
+*Pkg:  Debian dpkg interface (Priority 30)
+ Pkg:  Debian APT solver interface (Priority -1000)
+ S.L: 'deb' Standard Debian binary tree
+ S.L: 'deb-src' Standard Debian source tree
+ Idx: Debian Source Index
+ Idx: Debian Package Index
+ Idx: Debian Translation Index
+ Idx: Debian dpkg status file
+ Idx: EDSP scenario file""")
+        self.exit()
+        return
+
+    def do_help(self):
+	self.writeln("""apt 1.0.9.8.1 for amd64 compiled on Jun 10 2015 09:42:06
+Usage: apt-get [options] command
+       apt-get [options] install|remove pkg1 [pkg2 ...]
+       apt-get [options] source pkg1 [pkg2 ...]
+
+apt-get is a simple command line interface for downloading and
+installing packages. The most frequently used commands are update
+and install.
+
+Commands:
+   update - Retrieve new lists of packages
+   upgrade - Perform an upgrade
+   install - Install new packages (pkg is libc6 not libc6.deb)
+   remove - Remove packages
+   autoremove - Remove automatically all unused packages
+   purge - Remove packages and config files
+   source - Download source archives
+   build-dep - Configure build-dependencies for source packages
+   dist-upgrade - Distribution upgrade, see apt-get(8)
+   dselect-upgrade - Follow dselect selections
+   clean - Erase downloaded archive files
+   autoclean - Erase old downloaded archive files
+   check - Verify that there are no broken dependencies
+   changelog - Download and display the changelog for the given package
+   download - Download the binary package into the current directory
+
+Options:
+  -h  This help text.
+  -q  Loggable output - no progress indicator
+  -qq No output except for errors
+  -d  Download only - do NOT install or unpack archives
+  -s  No-act. Perform ordering simulation
+  -y  Assume Yes to all queries and do not prompt
+  -f  Attempt to correct a system with broken dependencies in place
+  -m  Attempt to continue if archives are unlocatable
+  -u  Show a list of upgraded packages as well
+  -b  Build the source package after fetching it
+  -V  Show verbose version numbers
+  -c=? Read this configuration file
+  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
+See the apt-get(8), sources.list(5) and apt.conf(5) manual
+pages for more information and options.
+                       This APT has Super Cow Powers.""")
+        self.exit()
+        return
 
     @inlineCallbacks
     def do_install(self,*args):
