d9f343fdf31f7613f2c936825b5c5f60e6735c9e
Michel Oosterhof <michel@oosterhof.net>
2015-11-17 16:16:26 +0000

    remove unused imports. rewrite strip() usage

--- cowrie/core/auth.py
+++ cowrie/core/auth.py
@@ -1,10 +1,8 @@
 # Copyright (c) 2009-2014 Upi Tamminen <desaster@gmail.com>
 # See the COPYRIGHT file for more information
 
-import string
 import json
 from os import path
-from sys import modules
 from random import randint
 
 from zope.interface import implements
@@ -26,11 +24,11 @@ class UserDB(object):
 
         with open(self.userdb_file, 'r') as f:
             while True:
-                line = f.readline()
-                if not line:
+                rawline = f.readline()
+                if not rawline:
                     break
-
-                line = string.strip(line)
+                
+                line = rawline.strip()
                 if not line:
                     continue
 
