08cfc03632164b59f903d9d825cab5763ccfa6b2
Michel Oosterhof <michel@oosterhof.net>
2015-12-03 19:31:24 +0000

    twistedchecker style

--- cowrie/core/ttylog.py
+++ cowrie/core/ttylog.py
@@ -13,17 +13,27 @@ OP_OPEN, OP_CLOSE, OP_WRITE, OP_EXEC = 1, 2, 3, 4
 TYPE_INPUT, TYPE_OUTPUT, TYPE_INTERACT = 1, 2, 3
 
 def ttylog_write(logfile, len, direction, stamp, data=None):
+    """
+    """
     with open(logfile, 'ab') as f:
         sec, usec = int(stamp), int(1000000 * (stamp - int(stamp)))
         f.write(struct.pack('<iLiiLL', 3, 0, len, direction, sec, usec))
         f.write(data)
 
+
+
 def ttylog_open(logfile, stamp):
+    """
+    """
     with open(logfile, 'ab') as f:
         sec, usec = int(stamp), int(1000000 * (stamp - int(stamp)))
         f.write(struct.pack('<iLiiLL', 1, 0, 0, 0, sec, usec))
 
+
+
 def ttylog_close(logfile, stamp):
+    """
+    """
     with open(logfile, 'ab') as f:
         sec, usec = int(stamp), int(1000000 * (stamp - int(stamp)))
         f.write(struct.pack('<iLiiLL', 2, 0, 0, 0, sec, usec))
