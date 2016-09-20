910256686b30dfd938b3b2b203bc436d04db0ba2
Michel Oosterhof <michel@oosterhof.net>
2016-09-20 18:22:33 +0000

    typo fix, only impacts log message

--- cowrie/telnet/transport.py
+++ cowrie/telnet/transport.py
@@ -202,7 +202,7 @@ class CowrieTelnetTransport(TelnetTransport, TimeoutMixin):
         self.setTimeout(300)
 
         log.msg(eventid='cowrie.session.connect',
-           format='New connection: %(src_ip)s:%(src_port)s (%(dst_ip)s:%(dst_port)s) [session: S%(sessionno)s]',
+           format='New connection: %(src_ip)s:%(src_port)s (%(dst_ip)s:%(dst_port)s) [session: T%(sessionno)s]',
            src_ip=self.transport.getPeer().host, src_port=self.transport.getPeer().port,
            dst_ip=self.transport.getHost().host, dst_port=self.transport.getHost().port,
            session=self.transportId, sessionno='T'+str(sessionno))
