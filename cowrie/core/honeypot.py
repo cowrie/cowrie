99c4d516b166481cec59e4ae4730d3c16a3f5cb7
fe7ch <fetch06@hotmail.com>
2016-10-27 12:24:50 +0300

    Add support for '>>' redirection (#313)

--- cowrie/core/honeypot.py
+++ cowrie/core/honeypot.py
@@ -32,22 +32,29 @@ class HoneyPotCommand(object):
         self.errorWrite = self.protocol.pp.errReceived
         # MS-DOS style redirect handling, inside the command
         # TODO: handle >>, 2>, etc
-        if '>' in self.args:
+        if '>' in self.args or '>>' in self.args:
             self.writtenBytes = 0
             self.write = self.write_to_file
-            index = self.args.index(">")
+            if '>>' in self.args:
+                index = self.args.index('>>')
+            else:
+                index = self.args.index('>')
             self.outfile = self.fs.resolve_path(str(self.args[(index + 1)]), self.protocol.cwd)
             del self.args[index:]
-            self.safeoutfile = '%s/%s-%s-%s-redir_%s' % (
-                self.protocol.cfg.get('honeypot', 'download_path'),
-                time.strftime('%Y%m%d-%H%M%S'),
-                self.protocol.getProtoTransport().transportId,
-                self.protocol.terminal.transport.session.id,
-                re.sub('[^A-Za-z0-9]', '_', self.outfile))
-            perm = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
-            self.fs.mkfile(self.outfile, 0, 0, 0, stat.S_IFREG | perm)
-            with open(self.safeoutfile, 'a'):
-                self.fs.update_realfile(self.fs.getfile(self.outfile), self.safeoutfile)
+            p = self.fs.getfile(self.outfile)
+            if not p:
+                self.safeoutfile = '%s/%s-%s-%s-redir_%s' % (
+                    self.protocol.cfg.get('honeypot', 'download_path'),
+                    time.strftime('%Y%m%d-%H%M%S'),
+                    self.protocol.getProtoTransport().transportId,
+                    self.protocol.terminal.transport.session.id,
+                    re.sub('[^A-Za-z0-9]', '_', self.outfile))
+                perm = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
+                self.fs.mkfile(self.outfile, 0, 0, 0, stat.S_IFREG | perm)
+                with open(self.safeoutfile, 'a'):
+                    self.fs.update_realfile(self.fs.getfile(self.outfile), self.safeoutfile)
+            else:
+                self.safeoutfile = p[fs.A_REALFILE]
 
 
     def check_arguments(self, application, args):
