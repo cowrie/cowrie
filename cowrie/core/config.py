700fcf4850741b4e9c731defed19d4ca95a22e3a
Ben Lelonek <lelonek1@umbc.edu>
2016-12-29 08:07:52 -0500

    Use configparser's ExtendedInterpolation so config values can include values from other sections
    Also added defaults to allow using additional whitespace characters in config values

--- cowrie/core/config.py
+++ cowrie/core/config.py
@@ -7,8 +7,20 @@ This module contains ...
 
 import configparser
 
+
 def readConfigFile(cfgfile):
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(
+        interpolation=configparser.ExtendedInterpolation(),
+        defaults={
+            'empty': '',  # Prevent whitespace from being stripped: 'value: ${empty}' => 'value: '
+            '\\n': '\n',  # New line: '${\n}'
+            '\\r': '\r',  # Carriage return: '${\r}'
+            '\\r\\n': '\r\n',  # CRLF: '${\r\n}'
+            't\\r\\n': '\r\r\n',  # CRLF for telnet (see CowrieTelnetTransport.write()): '${t\r\n}'
+            ' ': ' ',  # A single space character: '${ }'
+            '\\t': '\t',  # Tab escape: '${\t}'
+            '\t': '\t',  # Literal tab character: '${	}'
+        })
     config.read(cfgfile)
     return config
 
