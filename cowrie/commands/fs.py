74916c1d72a326ce6e4e5d397c5f10fe6aa18157
fe7ch <fe7ch@users.noreply.github.com>
2017-01-27 09:42:07 +0300

    Fix paths for egrep/fgrep commands (#426)
    * Support grep/egrep/fgrep without full path
    
    * Fixed path for egrep/fgrep
    
    * There is no grep/egrep/fgrep in /usr/bin in standard cowrie's fs.pickle.

--- cowrie/commands/fs.py
+++ cowrie/commands/fs.py
@@ -124,9 +124,8 @@ class command_grep(HoneyPotCommand):
 
 
 commands['/bin/grep'] = command_grep
-commands['/usr/bin/grep'] = command_grep
-commands['/usr/bin/egrep'] = command_grep
-commands['/usr/bin/fgrep'] = command_grep
+commands['/bin/egrep'] = command_grep
+commands['/bin/fgrep'] = command_grep
 
 
 class command_tail(HoneyPotCommand):
