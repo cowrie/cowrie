0ccfe314dfb30d84881fe46b28799d09c755e32c
fe7ch <fe7ch@users.noreply.github.com>
2017-11-27 10:45:25 +0200

    Pipe fix (#614)
    * Fix crash if 'command' is not present in cmd
    
    * Print data received by cat command
    
    * command_sh: Add support for commands passed via PIPE
    
    * Add support for chaining commands with PIPEs
    
    * command_sh: fix execution flow with "-c" flag passed

--- cowrie/commands/base.py
+++ cowrie/commands/base.py
@@ -13,11 +13,12 @@ from twisted.python import failure, log
 
 from twisted.internet import error, reactor
 
-from cowrie.shell.honeypot import HoneyPotCommand,StdOutStdErrEmulationProtocol
+from cowrie.shell.honeypot import HoneyPotCommand, HoneyPotShell, StdOutStdErrEmulationProtocol
 from cowrie.core.auth import UserDB
 from cowrie.core import utils
 
 commands = {}
+
 
 class command_whoami(HoneyPotCommand):
     """
@@ -142,7 +143,7 @@ class command_echo(HoneyPotCommand):
         # FIXME: Wrap in exception, Python escape cannot handle single digit \x codes (e.g. \x1)
         try:
             # replace r'\\x' with r'\x'
-            s = ''.join(args).replace(b'\\\\x', b'\\x')
+            s = ' '.join(args).replace(b'\\\\x', b'\\x')
 
             # replace single character escape \x0 with \x00
             s = re.sub('(?<=\\\\)x([0-9a-fA-F])(?=\\\\|\"|\'|\s|$)', 'x0\g<1>', s)
@@ -531,24 +532,31 @@ class command_sh(HoneyPotCommand):
         """
         """
         if len(self.args) and self.args[0].strip() == '-c':
-            line = ' '.join(self.args)
-            cmd = self.args[1]
-            cmdclass = self.protocol.getCommand(cmd,
-                                                self.environ['PATH'].split(':'))
-            if cmdclass:
-                log.msg(eventid='cowrie.command.success',
-                        input=line,
-                        format='Command found: %(input)s')
-                command = StdOutStdErrEmulationProtocol(self.protocol,cmdclass,self.args[2:],self.input_data,None)
-                self.protocol.pp.insert_command(command)
-                # Place this here so it doesn't write out only if last statement
 
-                if self.input_data:
-                    self.write(self.input_data)
-            else:
-                log.msg(eventid='cowrie.command.failed',
-                        input=''.join(cmd), format='Command not found: %(input)s')
-                self.write('bash: %s: command not found\n' % (cmd))
+            line = ' '.join(self.args[1:])
+
+            # it might be sh -c 'echo "sometext"', so don't use line.strip('\'\"')
+            if (line[0] == '\'' and line[-1] == '\'') or (line[0] == '"' and line[-1] == '"'):
+                line = line[1:-1]
+
+            self.execute_commands(line)
+
+        elif self.input_data:
+            self.execute_commands(self.input_data)
+
+        # TODO: handle spawning multiple shells, support other sh flags
+
+    def execute_commands(self, cmds):
+
+        # self.input_data holds commands passed via PIPE
+        # create new HoneyPotShell for our a new 'sh' shell
+        self.protocol.cmdstack.append(HoneyPotShell(self.protocol, interactive=False))
+
+        # call lineReceived method that indicates that we have some commands to parse
+        self.protocol.cmdstack[-1].lineReceived(cmds)
+
+        # remove the shell
+        self.protocol.cmdstack.pop()
 
 
 commands['/bin/bash'] = command_sh
