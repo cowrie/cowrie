01c8c536ce9aab63eca1775a588cd2794c5b6fa7
小柊 <wyn1995@vip.qq.com>
2017-12-12 13:29:17 +0800

    fix bug.
    1.fix hpfeeds message 'ttylog' is always null.

--- cowrie/output/hpfeeds.py
+++ cowrie/output/hpfeeds.py
@@ -324,7 +324,7 @@ class Output(cowrie.core.output.Output):
         elif entry["eventid"] == 'cowrie.log.closed':
             # entry["ttylog"]
             with open( entry["ttylog"]) as ttylog:
-                self.meta['ttylog'] = ttylog.read().encode('hex')
+                self.meta[session]['ttylog'] = ttylog.read().encode('hex')
 
         elif entry["eventid"] == 'cowrie.session.closed':
             log.msg('publishing metadata to hpfeeds')
