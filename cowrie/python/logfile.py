c5f3fa5c2a07ef322ac648ede30ece5a8742ac13
Michel Oosterhof <michel@oosterhof.net>
2018-02-06 18:09:30 +0000

    New daily logging format for cowrie.log

--- cowrie/python/logfile.py
+++ cowrie/python/logfile.py
@@ -8,25 +8,28 @@ This module contains
 
 from __future__ import division, absolute_import
 
-import os
-import glob
-import time
-import stat
-
+from twisted.logger import textFileLogObserver
 from twisted.python import logfile
 
 
 class CowrieDailyLogFile(logfile.DailyLogFile):
-
+    """
+    Overload original Twisted with improved date formatting
+    """
     
     def suffix(self, tupledate):
         """
         Return the suffix given a (year, month, day) tuple or unixtime
-        Overload original Twisted with improved date formatting
         """
         try:
             return "{:02d}-{:02d}-{:02d}".format(tupledate[0], tupledate[1], tupledate[2])
         except:
             # try taking a float unixtime
             return '_'.join(map(str, self.toDate(tupledate)))
+
+def logger():
+    """
+    """
+    logfile = CowrieDailyLogFile("cowrie.log", "log")
+    return textFileLogObserver(logfile)
 
