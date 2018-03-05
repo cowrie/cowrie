83337a9f9f1b9249975f241268d4dbfc696cb6dd
Michel Oosterhof <michel@oosterhof.net>
2018-03-05 17:41:46 +0400

    chmod right file

--- cowrie/core/artifact.py
+++ cowrie/core/artifact.py
@@ -87,7 +87,7 @@ class Artifact:
             os.rename(self.fp.name, shasumFilename)
             umask = os.umask(0)
             os.umask(umask)
-            os.chmod(self.fp.name, 0o666 & ~umask)
+            os.chmod(shasumFilename, 0o666 & ~umask)
 
         # if size>0:
         #    linkName = self.artifactDir + "/" \
