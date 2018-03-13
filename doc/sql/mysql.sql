656235b73b188535007c34618901b62c3d30ce4c
oliveriandrea <oliveriandrea@gmail.com>
2018-03-13 13:21:29 +0100

    Update sqlscheme to support cowrie.session.file_download.failed
    Added the support to cowrie.session.file_download.failed into the database schemas

--- doc/sql/mysql.sql
+++ doc/sql/mysql.sql
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `downloads` (
   `session` CHAR( 32 ) NOT NULL,
   `timestamp` datetime NOT NULL,
   `url` text NOT NULL,
-  `outfile` text NOT NULL,
+  `outfile` text default NULL,
   `shasum` varchar(64) default NULL,
   PRIMARY KEY  (`id`),
   KEY `session` (`session`,`timestamp`)
