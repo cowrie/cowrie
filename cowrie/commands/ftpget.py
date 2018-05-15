f2b78955fb4bdfdec77bc71ab1b5435d59b4a3d9
fe7ch <fetch06@hotmail.com>
2018-05-15 12:16:30 +0300

    ftpget_command:
        [+] Implement wrapper around ftplib.FTP class in order to bind on address specified in cowrie.cfg (out_addr)
        [+] Save payload as Artifact
        [-] Clean up the code
    
    Conflicts:
    	cowrie/commands/ftpget.py

--- cowrie/commands/ftpget.py
+++ cowrie/commands/ftpget.py
@@ -4,21 +4,78 @@
 from __future__ import division, absolute_import
 
 import os
-import re
-import time
 import getopt
-import hashlib
 import socket
-from ftplib import FTP
+import ftplib
 
 from twisted.python import log
 
-from cowrie.shell.honeypot import HoneyPotCommand
-from cowrie.shell.fs import *
-
+from cowrie.core.artifact import Artifact
 from cowrie.core.config import CONFIG
+from cowrie.shell.honeypot import HoneyPotCommand
+
 
 commands = {}
+
+
+class FTP(object, ftplib.FTP):
+
+    def __init__(self, *args, **kwargs):
+        self.source_address = kwargs.pop("source_address", None)
+        super(FTP, self).__init__(*args, **kwargs)
+
+    def connect(self, host='', port=0, timeout=-999, source_address=None):
+        if host != '':
+            self.host = host
+        if port > 0:
+            self.port = port
+        if timeout != -999:
+            self.timeout = timeout
+        if source_address is not None:
+            self.source_address = source_address
+        self.sock = socket.create_connection((self.host, self.port), self.timeout,
+                                             self.source_address)
+        self.af = self.sock.family
+        self.file = self.sock.makefile('rb')
+        self.welcome = self.getresp()
+        return self.welcome
+
+    def ntransfercmd(self, cmd, rest=None):
+        size = None
+        if self.passiveserver:
+            host, port = self.makepasv()
+            conn = socket.create_connection((host, port), self.timeout,
+                                            self.source_address)
+            try:
+                if rest is not None:
+                    self.sendcmd("REST %s" % rest)
+                resp = self.sendcmd(cmd)
+                if resp[0] == '2':
+                    resp = self.getresp()
+                if resp[0] != '1':
+                    raise ftplib.error_reply, resp
+            except:
+                conn.close()
+                raise
+        else:
+            sock = self.makeport()
+            try:
+                if rest is not None:
+                    self.sendcmd("REST %s" % rest)
+                resp = self.sendcmd(cmd)
+                if resp[0] == '2':
+                    resp = self.getresp()
+                if resp[0] != '1':
+                    raise ftplib.error_reply, resp
+                conn, sockaddr = sock.accept()
+                if self.timeout is not socket._GLOBAL_DEFAULT_TIMEOUT:
+                    conn.settimeout(self.timeout)
+            finally:
+                sock.close()
+        if resp[:3] == '150':
+            size = ftplib.parse150(resp)
+        return conn, size
+
 
 class command_ftpget(HoneyPotCommand):
     """
@@ -39,7 +96,7 @@ Download a file via FTP
     def start(self):
         try:
             optlist, args = getopt.getopt(self.args, 'cvu:p:P:')
-        except getopt.GetoptError as err:
+        except getopt.GetoptError:
             self.help()
             self.exit()
             return
@@ -82,14 +139,11 @@ Download a file via FTP
 
         fakeoutfile = self.fs.resolve_path(self.local_file, self.protocol.cwd)
         path = os.path.dirname(fakeoutfile)
-        if not path or \
-                not self.fs.exists(path) or \
-                not self.fs.isdir(path):
+        if not path or not self.fs.exists(path) or not self.fs.isdir(path):
             self.write('ftpget: can\'t open \'%s\': No such file or directory' % self.local_file)
             self.exit()
             return
 
-        url = 'ftp://%s/%s' % (self.host, self.remote_path)
         self.download_path = CONFIG.get('honeypot', 'download_path')
 
         self.url_log = 'ftp://'
@@ -103,59 +157,49 @@ Download a file via FTP
             self.url_log = '{}:{}'.format(self.url_log, self.port)
         self.url_log = '{}/{}'.format(self.url_log, self.remote_path)
 
-        tmp_fname = '%s_%s_%s_%s' % \
-                    (time.strftime('%Y%m%d%H%M%S'),
-                     self.protocol.getProtoTransport().transportId,
-                     self.protocol.terminal.transport.session.id,
-                     re.sub('[^A-Za-z0-9]', '_', url))
-        self.safeoutfile = os.path.join(self.download_path, tmp_fname)
+        self.artifactFile = Artifact(self.local_file)
 
-        result = self.ftp_download(self.safeoutfile)
+        result = self.ftp_download()
+
+        self.artifactFile.close()
 
         if not result:
+            # log to cowrie.log
+            log.msg(format='Attempt to download file(s) from URL (%(url)s) failed',
+                    url=self.url_log)
+
             self.protocol.logDispatch(eventid='cowrie.session.file_download.failed',
                                       format='Attempt to download file(s) from URL (%(url)s) failed',
                                       url=self.url_log)
-            self.safeoutfile = None
             self.exit()
             return
 
-        if not os.path.isfile(self.safeoutfile):
-            log.msg("there's no file " + self.safeoutfile)
-            self.exit()
-            return
-
-        with open(self.safeoutfile, 'rb') as f:
-            shasum = hashlib.sha256(f.read()).hexdigest()
-            hash_path = os.path.join(self.download_path, shasum)
-
-        # If we have content already, delete temp file
-        if not os.path.exists(hash_path):
-            os.rename(self.safeoutfile, hash_path)
-        else:
-            os.remove(self.safeoutfile)
-            log.msg("Not storing duplicate content " + shasum)
+        # log to cowrie.log
+        log.msg(format='Downloaded URL (%(url)s) with SHA-256 %(shasum)s to %(outfile)s',
+                url=self.url_log,
+                outfile=self.artifactFile.shasumFilename,
+                shasum=self.artifactFile.shasum)
 
         self.protocol.logDispatch(eventid='cowrie.session.file_download',
                                   format='Downloaded URL (%(url)s) with SHA-256 %(shasum)s to %(outfile)s',
                                   url=self.url_log,
-                                  outfile=hash_path,
-                                  shasum=shasum)
-
-        # Link friendly name to hash
-        # os.symlink(shasum, self.safeoutfile)
-
-        self.safeoutfile = None
+                                  outfile=self.artifactFile.shasumFilename,
+                                  shasum=self.artifactFile.shasum)
 
         # Update the honeyfs to point to downloaded file
-        self.fs.mkfile(fakeoutfile, 0, 0, os.path.getsize(hash_path), 33188)
-        self.fs.update_realfile(self.fs.getfile(fakeoutfile), hash_path)
+        self.fs.mkfile(fakeoutfile, 0, 0, os.path.getsize(self.artifactFile.shasumFilename), 33188)
+        self.fs.update_realfile(self.fs.getfile(fakeoutfile), self.artifactFile.shasumFilename)
         self.fs.chown(fakeoutfile, self.protocol.user.uid, self.protocol.user.gid)
 
         self.exit()
 
-    def ftp_download(self, safeoutfile):
-        ftp = FTP()
+    def ftp_download(self):
+
+        out_addr = None
+        if CONFIG.has_option('honeypot', 'out_addr'):
+            out_addr = (CONFIG.get('honeypot', 'out_addr'), 0)
+
+        ftp = FTP(source_address=out_addr)
 
         # connect
         if self.verbose:
@@ -200,7 +244,7 @@ Download a file via FTP
 
         try:
             ftp.cwd(self.remote_dir)
-            ftp.retrbinary('RETR %s' % self.remote_file, open(safeoutfile, 'wb').write)
+            ftp.retrbinary('RETR %s' % self.remote_file, self.artifactFile.write)
         except Exception as e:
             log.msg('FTP retrieval failed: %s' % str(e))
             self.write('ftpget: unexpected server response to USER: %s\n' % str(e))
