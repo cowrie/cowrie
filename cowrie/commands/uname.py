2156c721964703e7e66dbc166af2058fb2ff24e9
Michel Oosterhof <michel@oosterhof.net>
2018-06-18 10:32:42 +0000

    rewrite uname. store config in [shell]

--- cowrie/commands/uname.py
+++ cowrie/commands/uname.py
@@ -1,18 +1,72 @@
-#
+# Copyright (c) 2010 Upi Tamminen <desaster@gmail.com>
+# See the COPYRIGHT file for more information
+
+"""
+uname command
+"""
 
 from __future__ import division, absolute_import
-
-from cowrie.core.config import CONFIG
 from configparser import NoOptionError
 
+from cowrie.core.config import CONFIG
 from cowrie.shell.honeypot import HoneyPotCommand
 
 commands = {}
 
-class command_uname(HoneyPotCommand):
+def hardware_platform():
+    """
+    """
+    try:
+        return CONFIG.get('shell', 'hardware_platform')
+    except NoOptionError:
+        return 'x86_64'
 
-    def help(self):
-        return '''Usage: uname [OPTION]...
+
+
+def kernel_name():
+    """
+    """
+    try:
+        return CONFIG.get('shell', 'kernel_name')
+    except NoOptionError:
+        return 'Linux'
+
+
+
+def kernel_version():
+    """
+    """
+    try:
+        return CONFIG.get('shell', 'kernel_version')
+    except NoOptionError:
+        return '3.2.0-4-amd64'
+
+
+
+def kernel_build_string():
+    """
+    """
+    try:
+        return CONFIG.get('shell', 'kernel_build_string')
+    except NoOptionError:
+        return '#1 SMP Debian 3.2.68-1+deb7u1'
+
+
+
+def operating_system():
+    """
+    """
+    try:
+        return CONFIG.get('shell', 'operating_system')
+    except NoOptionError:
+        return 'GNU/Linux'
+
+
+
+def uname_help():
+    """
+    """
+    return """Usage: uname [OPTION]...
 Print certain system information.  With no OPTION, same as -s.
 
   -a, --all                print all information, in the following order,
@@ -31,52 +85,43 @@ Print certain system information.  With no OPTION, same as -s.
 GNU coreutils online help: <http://www.gnu.org/software/coreutils/>
 Full documentation at: <http://www.gnu.org/software/coreutils/uname>
 or available locally via: info '(coreutils) uname invocation'\n
-'''
+"""
 
-    def hardware_platform(self):
-        try:
-            return CONFIG.get("honeypot", "hardware_platform")
-        except NoOptionError:
-            return 'x86_64'
 
-    def kernel_version(self):
-        try:
-            return CONFIG.get("honeypot", "kernel_version")
-        except NoOptionError:
-            return '3.2.0-4-amd64'
 
-    def kernel_build_string(self):
-        try:
-           return CONFIG.get("honeypot", "kernel_build_string")
-        except NoOptionError:
-           return '#1 SMP Debian 3.2.68-1+deb7u1'
-
-    def operating_system(self):
-        return 'GNU/Linux'
-
+class command_uname(HoneyPotCommand):
+    """
+    """
     def full_uname(self):
-        return 'Linux %s %s %s %s %s\n' % ( self.protocol.hostname,
-                                            self.kernel_version(),
-                                            self.kernel_build_string(),
-                                            self.hardware_platform(),
-                                            self.operating_system() )
+        """
+        """
+        return '{} {} {} {} {} {}\n'.format(kernel_name(),
+                                            self.protocol.hostname,
+                                            kernel_version(),
+                                            kernel_build_string(),
+                                            hardware_platform(),
+                                            operating_system())
 
 
     def call(self):
-        if len(self.args) and self.args[0].strip() in ('-a', '--all'):
+        """
+        TODO: getopt style parsing
+        """
+        if not self.args:
+            self.write('{}\n'.format(kernel_name()))
+        elif self.args[0].strip() in ('-a', '--all'):
             self.write(self.full_uname())
-        elif len(self.args) and self.args[0].strip() in ('-r', '--kernel-release'):
-            self.write( '%s\n' % self.kernel_version() )
-        elif len(self.args) and self.args[0].strip() in ('-o', '--operating-system'):
-            self.write( '%s\n' % self.operating_system() )
-        elif len(self.args) and self.args[0].strip() in ('-n', '--nodename'):
-            self.write( '%s\n' % self.protocol.hostname )
-        elif len(self.args) and self.args[0].strip() in ('-m', '--machine', '-p', '--processor', '-i', '--hardware-platform'):
-            self.write( '%s\n' % self.hardware_platform() )
-        elif len(self.args) and self.args[0].strip() in ('-h', '--help'):
-            self.write( self.help() )
-        else:
-            self.write('Linux\n')
+        elif self.args[0].strip() in ('-s', '--kernel-name'):
+            self.write('{}\n'.format(kernel_name()))
+        elif self.args[0].strip() in ('-r', '--kernel-release'):
+            self.write('{}\n'.format(kernel_version()))
+        elif self.args[0].strip() in ('-o', '--operating-system'):
+            self.write('{}\n'.format(operating_system()))
+        elif self.args[0].strip() in ('-n', '--nodename'):
+            self.write('{}\n'.format(self.protocol.hostname))
+        elif self.args[0].strip() in ('-m', '--machine', '-p', '--processor', '-i', '--hardware-platform'):
+            self.write('{}\n'.format(hardware_platform()))
+        elif self.args[0].strip() in ('-h', '--help'):
+            self.write(uname_help())
 
 commands['/bin/uname'] = command_uname
-
