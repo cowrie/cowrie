de0b1f93ba81facf599d7a5b26ddffb13ff74331
Michel Oosterhof <michel@oosterhof.net>
2018-06-22 09:33:23 +0000

    optionally accept bytes to write()

--- cowrie/insults/insults.py
+++ cowrie/insults/insults.py
@@ -97,8 +97,8 @@ class LoggingServerProtocol(insults.ServerProtocol):
         This is where internal data in string representation is converted
         to bytes to be sent back to the user
         """
-        #if not isinstance(data, bytes):
-        data = data.encode("utf-8")
+        if not isinstance(data, bytes):
+            data = data.encode("utf-8")
 
         if self.ttylogEnabled and self.ttylogOpen:
             ttylog.ttylog_write(self.ttylogFile, len(data),
