0401c66f31173950dbc766ab4fe23254c5c84275
Florian Pelgrim <florian.pelgrim@craneworks.de>
2018-06-26 19:18:06 +0200

    flake8 E1 cowrie/proxy/session.py
    Before:
    flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --select=E1 --statistics --filename="*proxy/session.py"
    ./cowrie/proxy/session.py:182:13: E128 continuation line under-indented for visual indent
    ./cowrie/proxy/session.py:213:13: E128 continuation line under-indented for visual indent
    ./cowrie/proxy/session.py:251:13: E128 continuation line under-indented for visual indent
    ./cowrie/proxy/session.py:261:13: E128 continuation line under-indented for visual indent
    ./cowrie/proxy/session.py:262:13: E128 continuation line under-indented for visual indent
    5     E128 continuation line under-indented for visual indent
    5

--- cowrie/proxy/session.py
+++ cowrie/proxy/session.py
@@ -178,8 +178,8 @@ class ProxySSHSession(channel.CowrieSSHChannel):
         channel.CowrieSSHChannel.channelOpen(self, specificData)
         return
         log.msg("channelOpen")
-        helper = endpoints._NewConnectionHelper(reactor, self.host, self.port,
-            self.user, self.keys, self.password, None, self.knownHosts, None)
+        helper = endpoints._NewConnectionHelper(reactor, self.host, self.port, self.user, self.keys,
+                                                self.password, None, self.knownHosts, None)
         log.msg("helper = "+repr(helper))
         d = helper.secureConnection()
         d.addCallback(self._cbConnect)
@@ -209,8 +209,7 @@ class ProxySSHSession(channel.CowrieSSHChannel):
         value, rest = getNS(rest)
         if rest:
             raise ValueError("Bad data given in env request")
-        log.msg(eventid='cowrie.client.var', format='request_env: %(name)s=%(value)s',
-            name=name, value=value)
+        log.msg(eventid='cowrie.client.var', format='request_env: %(name)s=%(value)s', name=name, value=value)
         # FIXME: This only works for shell, not for exec command
         if self.session:
             self.session.environ[name] = value
@@ -247,8 +246,8 @@ class ProxySSHSession(channel.CowrieSSHChannel):
         log.msg('request_exec "{}"'.format(cmd))
         pf = _ProtocolFactory(self.client.transport)
         #ep = endpoints.SSHCommandClientEndpoint.existingConnection(self.conn, cmd).connect(pf)
-        ep = endpoints.SSHCommandClientEndpoint.newConnection(reactor, cmd,
-            self.user, self.host, port=self.port, password=self.password).connect(pf)
+        ep = endpoints.SSHCommandClientEndpoint.newConnection(reactor, cmd, self.user, self.host,
+                                                              port=self.port, password=self.password).connect(pf)
         return 1
 
 
@@ -257,9 +256,8 @@ class ProxySSHSession(channel.CowrieSSHChannel):
         """
         log.msg('request_shell')
         pf = _ProtocolFactory(self.client.transport)
-        ep = endpoints.SSHShellClientEndpoint.newConnection(reactor,
-            self.user, self.host, port=self.port,
-            password=self.password).connect(pf)
+        ep = endpoints.SSHShellClientEndpoint.newConnection(reactor, self.user, self.host,
+                                                            port=self.port, password=self.password).connect(pf)
         return 1
 
 
