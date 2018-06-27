2c35413ee391ef7be4ed099400cae1a44667e687
Florian Pelgrim <florian.pelgrim@craneworks.de>
2018-06-27 15:09:41 +0200

    flake8 E1 cowrie/output/cuckoo.py
    Before:
    flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --select=E1 --statistics --filename="*cuckoo.py"
    ./cowrie/output/cuckoo.py:106:17: E128 continuation line under-indented for visual indent
    ./cowrie/output/cuckoo.py:107:17: E128 continuation line under-indented for visual indent
    ./cowrie/output/cuckoo.py:108:17: E128 continuation line under-indented for visual indent
    ./cowrie/output/cuckoo.py:124:29: E126 continuation line over-indented for hanging indent
    ./cowrie/output/cuckoo.py:126:25: E126 continuation line over-indented for hanging indent
    ./cowrie/output/cuckoo.py:127:25: E126 continuation line over-indented for hanging indent
    ./cowrie/output/cuckoo.py:144:29: E126 continuation line over-indented for hanging indent
    ./cowrie/output/cuckoo.py:146:25: E126 continuation line over-indented for hanging indent
    ./cowrie/output/cuckoo.py:147:25: E126 continuation line over-indented for hanging indent
    6     E126 continuation line over-indented for hanging indent
    3     E128 continuation line under-indented for visual indent
    9

--- cowrie/output/cuckoo.py
+++ cowrie/output/cuckoo.py
@@ -102,10 +102,18 @@ class Output(cowrie.core.output.Output):
         res = None
         try:
             print("Looking for tasks for: {}".format(sha256))
-            res = requests.get(urljoin(self.url_base, "/files/view/sha256/{}".format(sha256)),
+            res = requests.get(
+                urljoin(
+                    self.url_base,
+                    "/files/view/sha256/{}".format(sha256)
+                ),
                 verify=False,
-                auth=HTTPBasicAuth(self.api_user,self.api_passwd),
-                timeout=60)
+                auth=HTTPBasicAuth(
+                    self.api_user,
+                    self.api_passwd
+                ),
+                timeout=60
+            )
             if res and res.ok:
                 print("Sample found in Sandbox, with ID: {}".format(res.json().get("sample", {}).get("id", 0)))
                 res = True
@@ -120,11 +128,18 @@ class Output(cowrie.core.output.Output):
         """
         files = {"file": (fileName, open(artifact, "rb").read())}
         try:
-            res = requests.post(urljoin(self.url_base, "tasks/create/file").encode("utf-8"), files=files, auth=HTTPBasicAuth(
-                            self.api_user,
-                            self.api_passwd
-                        ),
-                        verify=False)
+            res = requests.post(
+                urljoin(
+                    self.url_base,
+                    "tasks/create/file"
+                ).encode("utf-8"),
+                files=files,
+                auth=HTTPBasicAuth(
+                    self.api_user,
+                    self.api_passwd
+                ),
+                verify=False
+            )
             if res and res.ok:
                 print("Cuckoo Request: {}, Task created with ID: {}".format(res.status_code, res.json()["task_id"]))
             else:
@@ -140,11 +155,18 @@ class Output(cowrie.core.output.Output):
         """
         data = {"url": scanUrl}
         try:
-            res = requests.post(urljoin(self.url_base, "tasks/create/url").encode("utf-8"), data=data, auth=HTTPBasicAuth(
-                            self.api_user,
-                            self.api_passwd
-                        ),
-                        verify=False)
+            res = requests.post(
+                urljoin(
+                    self.url_base,
+                    "tasks/create/url"
+                ).encode("utf-8"),
+                data=data,
+                auth=HTTPBasicAuth(
+                    self.api_user,
+                    self.api_passwd
+                ),
+                verify=False
+            )
             if res and res.ok:
                 print("Cuckoo Request: {}, Task created with ID: {}".format(res.status_code, res.json()["task_id"]))
             else:
