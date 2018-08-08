cda47340483b2c9acf2409a21ea959fc372b7a1a
unglaublicherdude <github@matthias-simonis.de>
2018-08-09 00:36:30 +0200

    Seperated by test-type.

--- integration-test/test_Ssh.py
+++ integration-test/test_Ssh.py
@@ -2,12 +2,12 @@ import unittest
 import docker
 import os
 import time
-import socket
+from paramiko import SSHClient, client, ssh_exception
 from ddt import ddt, data
 
 
 @ddt
-class test_DockerContainer(unittest.TestCase):
+class test_Ssh(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -23,7 +23,7 @@ class test_DockerContainer(unittest.TestCase):
         cls.container.reload()
         cls.container_ip = cls.container.attrs["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
         time.sleep(10)
-        super(test_DockerContainer, cls).setUpClass()
+        super(test_Ssh, cls).setUpClass()
 
     @classmethod
     def tearDownClass(cls):
@@ -31,10 +31,16 @@ class test_DockerContainer(unittest.TestCase):
         Remove our Testcontainerclient.
         """
         cls.container.stop()
-        super(test_DockerContainer, cls).tearDownClass()
+        super(test_Ssh, cls).tearDownClass()
 
-    @data(2222)  # , 2223) <- telnet not open yet. activate when the dockerfile supports boths ports
-    def test_ConnectToPort_ConnectionResultIsNotZero(self, value):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((self.container_ip, value))
-        self.assertEqual(result, 0, f"Port {value} not open.")
+    def test_LoginIntoSshWithRoot_LoginWillSucceed(self):
+        sshClient = SSHClient()
+        sshClient.set_missing_host_key_policy(client.AutoAddPolicy)
+        sshClient.connect(self.container_ip, port=2222, username="root", password="foobar")
+
+    def test_LoginIntoSshWithOtherUser_WillRaiseAuthenticationException(self):
+        sshClient = SSHClient()
+        sshClient.set_missing_host_key_policy(client.AutoAddPolicy)
+        self.assertRaises(
+            ssh_exception.AuthenticationException,
+            lambda: sshClient.connect(self.container_ip, port=2222, username="OtherUser", password="OtherPassword"))
