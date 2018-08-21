c2536b4d813e14da1cccb0243edd96f819e3bd94
Florian Pelgrim <florian.pelgrim@craneworks.de>
2018-08-21 17:32:18 +0200

    Removed unreachable code

--- src/cowrie/shell/fs.py
+++ src/cowrie/shell/fs.py
@@ -265,7 +265,6 @@ class HoneyPotFilesystem(object):
             dir = self.get_path(os.path.dirname(path.strip('/')))
         except IndexError:
             raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), path)
-            return False
         dir.append([os.path.basename(path), T_DIR, uid, gid, size, mode, ctime, [], None, None])
         self.newcount += 1
 
