cd480394dad66ccdec4d03d92c0931f49f36b55b
Michel Oosterhof <micheloosterhof@users.noreply.github.com>
2018-09-11 12:01:42 +0400

    Setuppy (#876)
    * setup.py work in progress. should install library now.
    * add incremental for versioning
    * Fix 'ls -d' switches (worked wrong way around) and capitalisation in error message.

--- setup.py
+++ setup.py
@@ -10,46 +10,48 @@ setup(
     maintainer_email="michel@oosterhof.net",
     keywords="ssh telnet honeypot",
     url="https://github.com/cowrie/cowrie",
-    packages=find_packages(),
+    packages=find_packages('src'),
     include_package_data=True,
+    package_dir={'': 'src'},
     package_data={'': ['*.md']},
+    use_incremental=True,
     scripts=[
-        "bin/fsctl",
-        "bin/asciinema",
-        "bin/cowrie",
-        "bin/createfs",
-        "bin/playlog"
+        'bin/fsctl',
+        'bin/asciinema',
+        'bin/cowrie',
+        'bin/createfs',
+        'bin/playlog'
+    ],
+    setup_requires=[
+        'incremental',
+        'click'
     ],
     install_requires=[
-        "twisted>=17.1.0",
-        "cryptography>=0.9.1",
-        "configparser",
-        "pyopenssl",
-        "pyparsing",
-        "packaging",
-        "appdirs>=1.4.0",
-        "python-pyasn1",
-        "python-gmpy2",
-        "python-mysqldb",
-        "klein>=15.0.0",
-        "treq>=15.0.0",
-        "python-dateutil",
-        "service_identity>=14.0.0"
+        'twisted>=17.1.0',
+        'cryptography>=0.9.1',
+        'configparser',
+        'pyopenssl',
+        'pyparsing',
+        'incremental',
+        'packaging',
+        'appdirs>=1.4.0',
+        'python-dateutil',
+        'service_identity>=14.0.0'
     ],
     entry_points={
-        'console_scripts': ['run-the-app = deployme:main']
+        'console_scripts': ['cowrie = cowrie.scripts.cowrie:run']
     },
 
     extras_require={
-        'csirtg': ["csirtgsdk>=0.0.0a17"],
-        'dshield': ["requests"],
-        'elasticsearch': ["pyes"],
-        'mysql': ["mysqlclient"],
-        'mongodb': ["pymongo"],
-        'rethinkdblog': ["rethinkdb"],
-        's3': ["botocore"],
-        'slack': ["slackclient"],
-        'splunklegacy': ["splunk-sdk"],
-        'influxdb': ["influxdb"]
+        'csirtg': ['csirtgsdk>=0.0.0a17'],
+        'dshield': ['requests'],
+        'elasticsearch': ['pyes'],
+        'mysql': ['mysqlclient'],
+        'mongodb': ['pymongo'],
+        'rethinkdblog': ['rethinkdb'],
+        's3': ['botocore'],
+        'slack': ['slackclient'],
+        'splunklegacy': ['splunk-sdk'],
+        'influxdb': ['influxdb']
     }
 )
---
+++ src/cowrie/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides cowrie version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update cowrie` to change this file.
+
+from incremental import Version
+
+__version__ = Version('cowrie', 1, 5, 0)
+__all__ = ["__version__"]
