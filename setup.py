0d80f9b1e066e3c48370317b84012bf4047b7fb8
Michel Oosterhof <micheloosterhof@users.noreply.github.com>
2018-09-18 23:17:11 +0400

    Setup (#892)
    * Set license, classifiers, packages / run setup build as part of CI workflow.

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name="Cowrie",
-    version="1.0",
     description="Cowrie SSH/Telnet Honeypot.",
+    author="Michel Oosterhof",
+    author_email="michel@oosterhof.net",
     maintainer="Michel Oosterhof",
     maintainer_email="michel@oosterhof.net",
     keywords="ssh telnet honeypot",
-    url="https://github.com/cowrie/cowrie",
-    packages=find_packages('src'),
+    url="https://www.cowrie.org/",
+    packages=['cowrie', 'twisted'],
     include_package_data=True,
     package_dir={'': 'src'},
     package_data={'': ['*.md']},
     use_incremental=True,
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
     scripts=[
         'bin/fsctl',
         'bin/asciinema',
@@ -22,6 +24,19 @@ setup(
         'bin/createfs',
         'bin/playlog'
     ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: No Input/Output (Daemon)',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License'
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Security',
+        ],
     setup_requires=[
         'incremental',
         'click'
@@ -38,10 +53,6 @@ setup(
         'python-dateutil',
         'service_identity>=14.0.0'
     ],
-    entry_points={
-        'console_scripts': ['cowrie = cowrie.scripts.cowrie:run']
-    },
-
     extras_require={
         'csirtg': ['csirtgsdk>=0.0.0a17'],
         'dshield': ['requests'],
