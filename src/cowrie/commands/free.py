1b56140e681a0a5fbc4f071f7168e7745e16ab61
robert <robert.hendriks1993@gmail.com>
2018-10-20 22:39:51 +0200

    Issue #725 | Made the free command a lot more dynamic

--- src/cowrie/commands/free.py
+++ src/cowrie/commands/free.py
@@ -13,21 +13,10 @@ from cowrie.shell.command import HoneyPotCommand
 
 commands = {}
 
-FREE_BYTES = """             total       used       free     shared    buffers     cached
-Mem:       8069256    7872920     196336          0     410340    5295748
--/+ buffers/cache:    2166832    5902424
-Swap:      3764220     133080    3631140"""
-
-FREE_MEGA = """             total       used       free     shared    buffers     cached
-Mem:          7880       7690        189          0        400       5171
--/+ buffers/cache:       2118       5761
-Swap:         3675        129       3546"""
-
-FREE_HUMAN = """             total       used       free     shared    buffers     cached
-Mem:          7.7G       7.5G       189M         0B       400M       5.1G
--/+ buffers/cache:       2.1G       5.6G
-Swap:         3.6G       129M       3.5G"""
-
+FREE_OUTPUT = """              total        used        free      shared  buff/cache   available
+Mem:{MemTotal:>15}{calc_total_used:>12}{MemFree:>12}{Shmem:>12}{calc_total_buffers_and_cache:>12}{MemAvailable:>12}
+Swap:{SwapTotal:>14}{calc_swap_used:>12}{SwapFree:>12}
+"""
 
 class command_free(HoneyPotCommand):
     """
@@ -52,17 +41,56 @@ class command_free(HoneyPotCommand):
                 return
         self.do_free()
 
-    def do_free(self, fmt='bytes'):
+    def do_free(self, fmt='kilobytes'):
         """
         print free statistics
         """
-        if fmt == 'bytes':
-            self.write('{0}\n'.format(FREE_BYTES))
-        elif fmt == 'megabytes':
-            self.write('{0}\n'.format(FREE_MEGA))
-        elif fmt == 'human':
-            self.write('{0}\n'.format(FREE_HUMAN))
 
+        # Get real host memstats and add the calculated fields
+        raw_mem_stats = self.get_free_stats()  
+        raw_mem_stats['calc_total_buffers_and_cache'] = raw_mem_stats['Buffers'] + raw_mem_stats['Cached']
+        raw_mem_stats['calc_total_used'] = raw_mem_stats['MemTotal'] - ( raw_mem_stats['MemFree'] + raw_mem_stats['calc_total_buffers_and_cache'] )
+        raw_mem_stats['calc_swap_used'] = raw_mem_stats['SwapTotal'] - raw_mem_stats['SwapFree']
+
+
+        if fmt == 'megabytes':
+            # Transform KB to MB
+            for key, value in raw_mem_stats.iteritems():
+                raw_mem_stats[key] = int(value / 1000)
+        elif fmt == 'human':
+            magnitude = ["B", "M", "G", "T", "Z"]
+            for key, value in raw_mem_stats.iteritems():
+                current_magnitude = 0
+
+                # Keep dividing until we get a sane magnitude
+                while(value >= 1000 and current_magnitude < len(magnitude)):
+                    value = round(float(value / 1000), 1)
+                    current_magnitude += 1
+
+                # Format to string and append value with new magnitude
+                raw_mem_stats[key] = str("{:g}{}".format(value, magnitude[current_magnitude]))
+
+        # Write the output to screen
+        self.write(FREE_OUTPUT.format(**raw_mem_stats))
+
+
+    def get_free_stats(self):
+      """
+      Get the free stats from /proc
+      """
+      needed_keys = ["Buffers", "Cached", "MemTotal", "MemFree", "SwapTotal", "SwapFree", "Shmem", "MemAvailable"]
+      mem_info_map = {}
+      with open('/proc/meminfo', 'r') as proc_file:
+        for line in proc_file:
+          tokens = line.split(':')
+
+          # Later we are going to do some math on those numbers, better not include uneeded keys for performance
+          if tokens[0] in needed_keys: 
+              mem_info_map[tokens[0]] = int(tokens[1].lstrip().split(' ')[0]);
+
+      # Got a map with all tokens from /proc/meminfo and sizes in KBs
+      return mem_info_map
+      
 
 commands['/usr/bin/free'] = command_free
 commands['free'] = command_free
