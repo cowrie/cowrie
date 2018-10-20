90c5a179ad4d1ce833634e1ceb5bfbb1f4643ce9
robert <robert.hendriks1993@gmail.com>
2018-10-20 23:02:49 +0200

    Issue #725 | Fixed intergration tests

--- src/cowrie/commands/free.py
+++ src/cowrie/commands/free.py
@@ -18,6 +18,7 @@ Mem:{MemTotal:>15}{calc_total_used:>12}{MemFree:>12}{Shmem:>12}{calc_total_buffe
 Swap:{SwapTotal:>14}{calc_swap_used:>12}{SwapFree:>12}
 """
 
+
 class command_free(HoneyPotCommand):
     """
     free
@@ -47,11 +48,12 @@ class command_free(HoneyPotCommand):
         """
 
         # Get real host memstats and add the calculated fields
-        raw_mem_stats = self.get_free_stats()  
+        raw_mem_stats = self.get_free_stats()
         raw_mem_stats['calc_total_buffers_and_cache'] = raw_mem_stats['Buffers'] + raw_mem_stats['Cached']
-        raw_mem_stats['calc_total_used'] = raw_mem_stats['MemTotal'] - ( raw_mem_stats['MemFree'] + raw_mem_stats['calc_total_buffers_and_cache'] )
+        raw_mem_stats['calc_total_used'] = raw_mem_stats['MemTotal'] - (
+            raw_mem_stats['MemFree'] + raw_mem_stats['calc_total_buffers_and_cache']
+        )
         raw_mem_stats['calc_swap_used'] = raw_mem_stats['SwapTotal'] - raw_mem_stats['SwapFree']
-
 
         if fmt == 'megabytes':
             # Transform KB to MB
@@ -73,24 +75,23 @@ class command_free(HoneyPotCommand):
         # Write the output to screen
         self.write(FREE_OUTPUT.format(**raw_mem_stats))
 
-
     def get_free_stats(self):
-      """
-      Get the free stats from /proc
-      """
-      needed_keys = ["Buffers", "Cached", "MemTotal", "MemFree", "SwapTotal", "SwapFree", "Shmem", "MemAvailable"]
-      mem_info_map = {}
-      with open('/proc/meminfo', 'r') as proc_file:
-        for line in proc_file:
-          tokens = line.split(':')
+        """
+        Get the free stats from /proc
+        """
+        needed_keys = ["Buffers", "Cached", "MemTotal", "MemFree", "SwapTotal", "SwapFree", "Shmem", "MemAvailable"]
+        mem_info_map = {}
+        with open('/proc/meminfo', 'r') as proc_file:
+            for line in proc_file:
+                tokens = line.split(':')
 
-          # Later we are going to do some math on those numbers, better not include uneeded keys for performance
-          if tokens[0] in needed_keys: 
-              mem_info_map[tokens[0]] = int(tokens[1].lstrip().split(' ')[0]);
+                # Later we are going to do some math on those numbers, better not include uneeded keys for performance
+                if tokens[0] in needed_keys:
+                    mem_info_map[tokens[0]] = int(tokens[1].lstrip().split(' ')[0])
 
-      # Got a map with all tokens from /proc/meminfo and sizes in KBs
-      return mem_info_map
-      
+        # Got a map with all tokens from /proc/meminfo and sizes in KBs
+        return mem_info_map
+
 
 commands['/usr/bin/free'] = command_free
 commands['free'] = command_free
