ba01137534238df60e2ff415b9814340647a3656
Michel Oosterhof <micheloosterhof@users.noreply.github.com>
2018-10-29 17:47:00 +0400

    Fix W605 (#944)
    * Fix W605 and bytes/string issues in echo -e

--- src/cowrie/commands/base.py
+++ src/cowrie/commands/base.py
@@ -137,10 +137,10 @@ class command_echo(HoneyPotCommand):
 
         try:
             # replace r'\\x' with r'\x'
-            string = ' '.join(args).replace('\\\\x', '\\x')
+            string = ' '.join(args).replace(r'\\x', r'\x')
 
             # replace single character escape \x0 with \x00
-            string = re.sub('(?<=\\\\)x([0-9a-fA-F])(?=\\\\|\"|\'|\s|$)', 'x0\g<1>', string)
+            string = re.sub(r'(?<=\\)x([0-9a-fA-F])(?=\\|\"|\'|\s|$)', r'x0\g<1>', string)
 
             # if the string ends with \c escape, strip it and set newline flag to False
             if string.endswith('\\c'):
@@ -152,8 +152,9 @@ class command_echo(HoneyPotCommand):
 
             if escape_decode:
                 string = codecs.escape_decode(string)[0]
-
-            self.write(string)
+                self.writeBytes(string)
+            else:
+                self.write(string)
 
         except ValueError:
             log.msg("echo command received Python incorrect hex escape")
@@ -174,7 +175,7 @@ class command_printf(HoneyPotCommand):
                 s = ''.join(self.args[0]).replace('\\\\x', '\\x')
 
                 # replace single character escape \x0 with \x00
-                s = re.sub('(?<=\\\\)x([0-9a-fA-F])(?=\\\\|\"|\'|\s|$)', 'x0\g<1>', s)
+                s = re.sub(r'(?<=\\)x([0-9a-fA-F])(?=\\|\"|\'|\s|$)', r'x0\g<1>', s)
 
                 # strip single and double quotes
                 s = s.strip('\"\'')
