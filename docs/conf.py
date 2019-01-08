b92c799a117356ac167f06fc33e54c11797c68bb
Michel Oosterhof <michel@oosterhof.net>
2019-01-08 12:34:26 +0400

    add cowrie version in there automatically

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -28,6 +28,7 @@ version = '1.5'
 # The full version, including alpha/beta/rc tags
 release = ''
 
+from cowrie import version as cowrie_version_object
 
 # -- General configuration ---------------------------------------------------
 
