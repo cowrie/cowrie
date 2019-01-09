7393cb38157102437f6d469402b5b6363e79ceba
Michel Oosterhof <michel@oosterhof.net>
2019-01-09 11:18:41 +0400

    sphinx conf is flake8 compliant

--- docs/conf.py
+++ docs/conf.py
@@ -14,9 +14,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
-from cowrie import version as cowrie_version_object
 
+# The Makefile sets PYTHONPATH so this module is available
+from cowrie import version as cowrie_version_object
 
 # -- Project information -----------------------------------------------------
 
