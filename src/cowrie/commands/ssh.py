e628cda3a826afb6c807ae4ba210ab52d95c3e24
Mehtab Zafar <mehtab.zafar98@gmail.com>
2019-03-08 01:15:24 +0530

    Support Vairable SSH version (#1023)

--- src/cowrie/commands/ssh.py
+++ src/cowrie/commands/ssh.py
@@ -12,9 +12,21 @@ import time
 from twisted.internet import reactor
 from twisted.python import log
 
+from cowrie.core.config import CONFIG
 from cowrie.shell.command import HoneyPotCommand
 
 commands = {}
+
+
+OUTPUT = [
+    'usage: ssh [-46AaCfGgKkMNnqsTtVvXxYy] [-B bind_interface]',
+    '           [-b bind_address] [-c cipher_spec] [-D [bind_address:]port]',
+    '           [-E log_file] [-e escape_char] [-F configfile] [-I pkcs11]',
+    '           [-i identity_file] [-J [user@]host[:port]] [-L address]',
+    '           [-l login_name] [-m mac_spec] [-O ctl_cmd] [-o option] [-p port]',  # noqa
+    '           [-Q query_option] [-R address] [-S ctl_path] [-W host:port]',
+    '           [-w local_tun[:remote_tun]] destination [command]'
+]
 
 
 class command_ssh(HoneyPotCommand):
@@ -28,24 +40,20 @@ class command_ssh(HoneyPotCommand):
 
     def start(self):
         try:
-            optlist, args = getopt.getopt(self.args, '-1246AaCfgKkMNnqsTtVvXxYb:c:D:e:F:i:L:l:m:O:o:p:R:S:w:')
+            options = '-1246AaCfgKkMNnqsTtVvXxYb:c:D:e:F:i:L:l:m:O:o:p:R:S:w:'
+            optlist, args = getopt.getopt(self.args, options)
         except getopt.GetoptError:
             self.write('Unrecognized option\n')
             self.exit()
         for opt in optlist:
             if opt[0] == '-V':
-                self.write('OpenSSH_6.7p1 Debian-5, OpenSSL 1.0.1k 8 Jan 2015\n')
+                self.write(CONFIG.get('ssh', 'version',
+                                      fallback="OpenSSH_7.9p1, \
+                                      OpenSSL 1.1.1a  20 Nov 2018\n"))
                 self.exit()
                 return
         if not len(args):
-            for l in (
-                    'usage: ssh [-1246AaCfgKkMNnqsTtVvXxY] [-b bind_address] [-c cipher_spec]',
-                    '           [-D [bind_address:]port] [-e escape_char] [-F configfile]',
-                    '           [-i identity_file] [-L [bind_address:]port:host:hostport]',
-                    '           [-l login_name] [-m mac_spec] [-O ctl_cmd] [-o option] [-p port]',
-                    '           [-R [bind_address:]port:host:hostport] [-S ctl_path]',
-                    '           [-w local_tun[:remote_tun]] [user@]hostname [command]',
-            ):
+            for l in OUTPUT:
                 self.write('{0}\n'.format(l))
             self.exit()
             return
@@ -60,22 +68,27 @@ class command_ssh(HoneyPotCommand):
             if self.valid_ip(host):
                 self.ip = host
             else:
-                self.write('ssh: Could not resolve hostname %s: Name or service not known\n' % (host,))
+                self.write('ssh: Could not resolve hostname %s: \
+                    Name or service not known\n' % (host,))
                 self.exit()
         else:
             s = hashlib.md5(host).hexdigest()
-            self.ip = '.'.join([str(int(x, 16)) for x in (s[0:2], s[2:4], s[4:6], s[6:8])])
+            self.ip = '.'.join([str(int(x, 16)) for x in
+                                (s[0:2], s[2:4], s[4:6], s[6:8])])
 
         self.host = host
         self.user = user
 
-        self.write('The authenticity of host \'%s (%s)\' can\'t be established.\n' % (self.host, self.ip))
-        self.write('RSA key fingerprint is 9d:30:97:8a:9e:48:0d:de:04:8d:76:3a:7b:4b:30:f8.\n')
+        self.write('The authenticity of host \'%s (%s)\' \
+            can\'t be established.\n' % (self.host, self.ip))
+        self.write('RSA key fingerprint is \
+            9d:30:97:8a:9e:48:0d:de:04:8d:76:3a:7b:4b:30:f8.\n')
         self.write('Are you sure you want to continue connecting (yes/no)? ')
         self.callbacks = [self.yesno, self.wait]
 
     def yesno(self, line):
-        self.write('Warning: Permanently added \'{}\' (RSA) to the list of known hosts.\n'.format(self.host))
+        self.write('Warning: Permanently added \'{}\' (RSA) to the \
+            list of known hosts.\n'.format(self.host))
         self.write('%s@%s\'s password: ' % (self.user, self.host))
         self.protocol.password_input = True
 
@@ -93,8 +106,10 @@ class command_ssh(HoneyPotCommand):
         if not self.fs.exists(self.protocol.cwd):
             self.protocol.cwd = '/'
         self.protocol.password_input = False
-        self.write('Linux {} 2.6.26-2-686 #1 SMP Wed Nov 4 20:45:37 UTC 2009 i686\n'.format(self.protocol.hostname))
-        self.write('Last login: %s from 192.168.9.4\n' % (time.ctime(time.time() - 123123),))
+        self.write('Linux {} 2.6.26-2-686 #1 SMP Wed Nov 4 20:45:37 \
+            UTC 2009 i686\n'.format(self.protocol.hostname))
+        self.write('Last login: %s from 192.168.9.4\n'
+                   % (time.ctime(time.time() - 123123),))
         self.exit()
 
     def lineReceived(self, line):
