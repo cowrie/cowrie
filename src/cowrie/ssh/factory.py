ca45ef8d079b9f4e3a1d32926fa12caa70f25599
Mayank Sharma <mayanksha@users.noreply.github.com>
2019-03-14 11:44:15 +0530

    Expose SSH key exchange parameters in config file (#1051)
    * Added support for getting encryption, compression, and hash methods from
    config file

--- src/cowrie/ssh/factory.py
+++ src/cowrie/ssh/factory.py
@@ -107,21 +107,45 @@ class CowrieSSHFactory(factory.SSHFactory):
                 log.msg("No moduli, no diffie-hellman-group-exchange-sha256")
             t.supportedKeyExchanges = ske
 
-        # Reorder supported ciphers to resemble current openssh more
-        t.supportedCiphers = [
-            b'aes128-ctr',
-            b'aes192-ctr',
-            b'aes256-ctr',
-            b'aes128-cbc',
-            b'3des-cbc',
-            b'blowfish-cbc',
-            b'cast128-cbc',
-            b'aes192-cbc',
-            b'aes256-cbc'
-        ]
+        try:
+            t.supportedCiphers = [bytearray(i, 'utf-8') for i in CONFIG.get('ssh', 'ciphers').split(',')]
+        except NoOptionError:
+            # Reorder supported ciphers to resemble current openssh more
+            t.supportedCiphers = [
+                b'aes128-ctr',
+                b'aes192-ctr',
+                b'aes256-ctr',
+                b'aes256-cbc',
+                b'aes192-cbc',
+                b'aes128-cbc',
+                b'3des-cbc',
+                b'blowfish-cbc',
+                b'cast128-cbc',
+            ]
+
+        try:
+            t.supportedMACs = [bytearray(i, 'utf-8') for i in CONFIG.get('ssh', 'macs').split(',')]
+        except NoOptionError:
+            # SHA1 and MD5 are considered insecure now. Use better algos
+            # like SHA-256 and SHA-384
+            t.supportedMACs = [
+                    b'hmac-sha2-512',
+                    b'hmac-sha2-384',
+                    b'hmac-sha2-256',
+                    b'hmac-sha1',
+                    b'hmac-md5'
+                ]
+
+        try:
+            t.supportedCompressions = [bytearray(i, 'utf-8') for i in CONFIG.get('ssh', 'compression').split(',')]
+        except NoOptionError:
+            t.supportedCompressions = [b'zlib@openssh.com', b'zlib', b'none']
+
+        # TODO: Newer versions of SSH will use ECDSA keys too as mentioned
+        # at https://tools.ietf.org/html/draft-miller-ssh-agent-02#section-4.2.2
+        #
+        # Twisted only supports below two keys
         t.supportedPublicKeys = [b'ssh-rsa', b'ssh-dss']
-        t.supportedMACs = [b'hmac-md5', b'hmac-sha1']
-        t.supportedCompressions = [b'zlib@openssh.com', b'zlib', b'none']
 
         t.factory = self
         return t
