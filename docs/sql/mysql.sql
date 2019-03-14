0c61ffd9c5eae8009255b3350315a5eb684397ea
Guilherme Borges <g.borges@campus.fct.unl.pt>
2019-03-14 12:03:02 +0000

    add update file and session constraint

--- docs/sql/mysql.sql
+++ docs/sql/mysql.sql
@@ -80,19 +80,22 @@ CREATE INDEX arch_index ON params (arch);
 
 CREATE TABLE IF NOT EXISTS `ipforwards` (
   `id` int(11) NOT NULL auto_increment,
-  `session` CHAR( 32 ) NOT NULL,
+  `session` CHAR(32) NOT NULL,
   `timestamp` datetime NOT NULL,
   `dst_ip` varchar(15) NOT NULL default '',
   `dst_port` int(5) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  FOREIGN KEY (`session`) REFERENCES `sessions`(`id`)
 ) ;
 
 CREATE TABLE IF NOT EXISTS `ipforwardsdata` (
   `id` int(11) NOT NULL auto_increment,
-  `session` CHAR( 32 ) NOT NULL,
+  `session` CHAR(32) NOT NULL,
   `timestamp` datetime NOT NULL,
   `dst_ip` varchar(15) NOT NULL default '',
   `dst_port` int(5) NOT NULL,
   `data` text NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  KEY `session` (`session`,`timestamp`),
+  FOREIGN KEY (`session`) REFERENCES `sessions`(`id`)
 ) ;
