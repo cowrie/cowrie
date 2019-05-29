db2c6a1bc93b88a7f000a731f947820f84487c18
Tom <tbowyer@outlook.com>
2019-05-29 13:13:11 -0500

    adding geoip

--- src/cowrie/output/omniscidb.py
+++ src/cowrie/output/omniscidb.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import, division
 
+import geoip2.database
+
 import pymapd as pmd
 
 from twisted.python import log
@@ -26,6 +28,13 @@ class CowrieOutput():
         self.shasum = None
         self.download_result = None
         self.was_upload = None
+        self.src_lon = None
+        self.src_lat = None
+        self.src_city = None
+        self.src_state = None
+        self.src_zip_code = None
+        self.src_country = None
+        self.src_country_iso = None
 
     def make_tuple(self):
         return (self.session,
@@ -41,8 +50,24 @@ class CowrieOutput():
                 self.outfile,
                 self.shasum,
                 self.download_result,
-                self.was_upload
+                self.was_upload,
+                self.src_lon,
+                self.src_lat,
+                self.src_city,
+                self.src_state,
+                self.src_zip_code,
+                self.src_country,
+                self.src_country_iso
                 )
+
+    def set_src_geo(self, src_geo_tuple):
+        self.src_lon = src_geo_tuple[0]
+        self.src_lat = src_geo_tuple[1]
+        self.src_city = src_geo_tuple[2]
+        self.src_state = src_geo_tuple[3]
+        self.src_zip_code = src_geo_tuple[4]
+        self.src_country = src_geo_tuple[5]
+        self.src_country_iso = src_geo_tuple[6]
 
 
 class Output(cowrie.core.output.Output):
@@ -63,6 +88,8 @@ class Output(cowrie.core.output.Output):
             'output_omniscidb', 'username', fallback=None)
         self.password = CowrieConfig().get(
             'output_omniscidb', 'password', fallback=None)
+        self.mmdb_location = CowrieConfig().get(
+            'output_omniscidb', 'mmdb_location', fallback=None)
         try:
             self.connection = pmd.connect(user=self.username,
                                           password=self.password,
@@ -73,6 +100,9 @@ class Output(cowrie.core.output.Output):
             self.cursor = self.connection.cursor()
         except Exception as e:
             log.msg("Failed to login to OmniSciDB: got error {0}".format(e))
+
+        if self.mmdb_location is not None:
+            self.mmdb_geo = geoip2.database.Reader(self.mmdb_location)
 
         try:
             # Create our Tables for Cowrie
@@ -90,10 +120,31 @@ class Output(cowrie.core.output.Output):
                 outfile TEXT ENCODING DICT(32),\
                 shasum TEXT ENCODING DICT(32),\
                 download_result TEXT ENCODING DICT(32),\
-                was_upload TEXT ENCODING DICT(32))")
+                was_upload TEXT ENCODING DICT(32),\
+                src_lon float,\
+                src_lat float,\
+                src_city TEXT ENCODING DICT(32),\
+                src_state TEXT ENCODING DICT(32),\
+                src_zip_code TEXT ENCODING DICT(32),\
+                src_country TEXT ENCODING DICT(16),\
+                src_country_iso TEXT ENCODING DICT(16))")
 
         except Exception as e:
             log.msg("Failed to create table got error {0}".format(e))
+
+    def maxmind_geo_lookup(self, reader, ip):
+        try:
+            response = reader.city(ip)
+            return (float(response.location.longitude),
+                    float(response.location.latitude),
+                    str(response.city.name),
+                    str(response.subdivisions.most_specific.name),
+                    str(response.postal.code),
+                    str(response.country.name),
+                    str(response.country.iso_code))
+        except Exception as e:
+            log.msg("Failed to lookup ip {0}".format(e))
+            return None
 
     def load_data(self, data_dict):
         try:
@@ -122,6 +173,14 @@ class Output(cowrie.core.output.Output):
             cowrie_output.ip_address = entry["src_ip"]
             cowrie_output.username = entry['username']
             cowrie_output.password = entry['password']
+
+            # If Maxmind is loaded set the geoip tuple.
+            if self.mmdb_geo:
+                src_geo_tuple = (
+                    self.maxmind_geo_lookup(
+                        self.mmdb_geo, cowrie_output.ip_address))
+                if src_geo_tuple is not None:
+                    cowrie_output.set_src_geo(src_geo_tuple)
 
             if entry["eventid"] == 'cowrie.login.success':
                 cowrie_output.login_result = "1"
