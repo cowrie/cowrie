34f846473217636f592f0fc8e13f448ceeb3871f
Guilherme Borges <guilhermerosasborges@gmail.com>
2019-07-01 18:41:03 +0100

    Add a Telnet proxy for Cowrie (#1159)
    * add telnet proxy

---
+++ src/cowrie/telnet/factory.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2015, 2016 GoSecure Inc.
+"""
+Telnet Transport and Authentication for the Honeypot
+
+@author: Olivier Bilodeau <obilodeau@gosecure.ca>
+"""
+
+from __future__ import absolute_import, division
+
+import time
+
+
+from twisted.internet import protocol
+from twisted.python import log
+
+from cowrie.core.config import CowrieConfig
+from cowrie.telnet.transport import CowrieTelnetTransport
+from cowrie.telnet.userauth import HoneyPotTelnetAuthProtocol
+from cowrie.telnet_proxy.server_transport import FrontendTelnetTransport
+
+
+class HoneyPotTelnetFactory(protocol.ServerFactory):
+    """
+    This factory creates HoneyPotTelnetAuthProtocol instances
+    They listen directly to the TCP port
+    """
+    tac = None
+
+    # TODO logging clarity can be improved: see what SSH does
+    def logDispatch(self, *msg, **args):
+        """
+        Special delivery to the loggers to avoid scope problems
+        """
+        args['sessionno'] = 'T{0}'.format(str(args['sessionno']))
+        for output in self.tac.output_plugins:
+            output.logDispatch(*msg, **args)
+
+    def startFactory(self):
+        try:
+            honeyfs = CowrieConfig().get('honeypot', 'contents_path')
+            issuefile = honeyfs + "/etc/issue.net"
+            self.banner = open(issuefile, 'rb').read()
+        except IOError:
+            self.banner = b""
+
+        # For use by the uptime command
+        self.starttime = time.time()
+
+        # hook protocol
+        if CowrieConfig().get('honeypot', 'backend', fallback='shell') == 'proxy':
+            self.protocol = lambda: FrontendTelnetTransport()
+        else:
+            self.protocol = lambda: CowrieTelnetTransport(HoneyPotTelnetAuthProtocol, self.portal)
+
+        protocol.ServerFactory.startFactory(self)
+        log.msg("Ready to accept Telnet connections")
+
+    def stopFactory(self):
+        """
+        Stop output plugins
+        """
+        protocol.ServerFactory.stopFactory(self)
