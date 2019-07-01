a2f4ff1675633b01509fc556100a6d6e0201e86a
Guilherme Borges <guilhermerosasborges@gmail.com>
2019-07-01 21:12:28 +0100

    add pool interface from proxy branch

---
+++ src/cowrie/pool_interface/client.py
@@ -0,0 +1,71 @@
+import struct
+
+from twisted.internet.protocol import ClientFactory, Protocol
+from twisted.python import log
+
+
+class PoolClient(Protocol):
+    def __init__(self, factory):
+        self.factory = factory
+        self.parent = None
+        self.vm_id = None  # used when disconnecting
+
+    def connectionMade(self):
+        pass
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def send_initialisation(self):
+        """
+        Used only by the PoolHandler on the first connection, to set the pool up.
+        """
+        buf = struct.pack('!cII', b'i', 10, 600)
+        self.transport.write(buf)
+
+    def send_vm_request(self, src_ip):
+        fmt = '!cH{0}s'.format(len(src_ip))
+        buf = struct.pack(fmt, b'r', len(src_ip), src_ip.encode())
+
+        self.transport.write(buf)
+
+    def send_vm_free(self):
+        buf = struct.pack('!cI', b'f', self.vm_id)
+        self.transport.write(buf)
+
+    def dataReceived(self, data):
+        # only makes sense to process data if we have a parent to send it to
+        if not self.parent:
+            log.err('Parent not set, discarding data from pool')
+            return
+
+        response = struct.unpack('!cI', data[0:5])
+
+        res_op = response[0]
+        res_code = response[1]
+
+        if res_op == b'i':
+            # callback to the handler to signal that pool was initialised successfully,
+            # so that SSH and Telnet setup can proceed
+            self.parent.initialisation_response(res_code)
+
+        elif res_op == b'r':
+            recv = struct.unpack('!IH', data[5:11])
+            self.vm_id = recv[0]
+            ip_len = recv[1]
+
+            recv = struct.unpack('!{0}sHH'.format(ip_len), data[11:])
+            honey_ip = recv[0]
+            ssh_port = recv[1]
+            telnet_port = recv[2]
+
+            self.parent.received_pool_data(res_op, res_code, honey_ip, ssh_port, telnet_port)
+
+
+class PoolClientFactory(ClientFactory):
+    def __init__(self, handler):
+        pass
+        # self.handler = handler
+
+    def buildProtocol(self, addr):
+        return PoolClient(self)
