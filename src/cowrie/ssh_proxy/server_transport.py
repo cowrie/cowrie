c6daa62a3b5facae7db67f761186a7bd63663d7d
Guilherme Borges <guilhermerosasborges@gmail.com>
2019-07-23 23:57:15 +0100

    fix variable in ssh_proxy

--- src/cowrie/ssh_proxy/server_transport.py
+++ src/cowrie/ssh_proxy/server_transport.py
@@ -328,8 +328,8 @@ class FrontendSSHTransport(transport.SSHServerTransport, TimeoutMixin):
         """
         self.setTimeout(None)
 
-        if self.client and self.client.transport:
-            self.client.transport.loseConnection()
+        if self.sshParse.client and self.sshParse.client.transport:
+            self.sshParse.client.transport.loseConnection()
 
         transport.SSHServerTransport.connectionLost(self, reason)
 
