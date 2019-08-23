f11ea18828c1a5d93bb6f7de682cabd5670c1525
Guilherme Borges <guilhermerosasborges@gmail.com>
2019-08-23 22:34:32 +0100

    add log msg

--- src/backend_pool/libvirt/snapshot_handler.py
+++ src/backend_pool/libvirt/snapshot_handler.py
@@ -5,13 +5,14 @@ import getpass
 import shutil
 import subprocess
 
+from twisted.python import log
+
 
 def create_disk_snapshot(source_img, destination_img):
     try:
         shutil.chown(source_img, getpass.getuser())
     except Exception:  # TODO should be PermissionError under python 3, but python 2 does not have it
-        pass
-        # log.msg('Need root to create snapshot')
+        log.msg('Should have root to create snapshot')
 
     out = subprocess.run(['qemu-img', 'create', '-f', 'qcow2', '-b', source_img, destination_img], capture_output=True)
     return out.returncode == 0
