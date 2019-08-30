95bdde59caebfbaa300c54a8f96174c6e51a6b33
Michel Oosterhof <michel@oosterhof.net>
2019-08-30 15:47:09 +0400

    crashreporter!

--- src/cowrie/output/crashreporter.py
+++ src/cowrie/output/crashreporter.py
@@ -1,36 +1,30 @@
 """
 Cowrie Crashreport
+
+This output plugin is not like the others. 
+It has its own emit() function and does not use cowrie eventid's to avoid circular calls
 """
 
 from __future__ import absolute_import, division
 
+import json
 import treq
 
 from twisted.internet import defer, error
 from twisted.python import log
+from twisted.logger._levels import LogLevel
 
 import cowrie.core.output
 from cowrie.core.config import CowrieConfig
 from cowrie._version import __version__
 
 COWRIE_USER_AGENT = 'Cowrie Honeypot {}'.format(__version__)
-COWRIE_URL = 'http://api.cowrie.org:8888/v1/'
-
-"""
-from twisted.logger._levels import LogLevel
-
-def analyze(event):
-    if event.get("log_level") == LogLevel.critical:
-        print "Stopping for: ", event
-        reactor.stop()
-
-globalLogPublisher.addObserver(analyze)
-"""
-
+COWRIE_URL = 'https://api.cowrie.org:8888/v1/'
+COWRIE_URL = 'http://127.0.0.1:8888/v1/'
 
 class Output(cowrie.core.output.Output):
     """
-    Cowrie Telemetry output
+    Cowrie Crashreporter output
     """
 
     def start(self):
@@ -40,6 +34,13 @@ class Output(cowrie.core.output.Output):
         self.apiKey = CowrieConfig().get('output_cowrie', 'api_key', fallback=None)
         self.debug = CowrieConfig().getboolean('output_cowrie', 'debug', fallback=False)
 
+    def emit(self, event):
+        """
+        Note we override emit() here, unlike other plugins.
+        """
+        if event.get('log_level') == LogLevel.critical:
+            self.crashreport(event)
+
     def stop(self):
         """
         Stop output plugin
@@ -47,11 +48,17 @@ class Output(cowrie.core.output.Output):
         pass
 
     def write(self, entry):
-        if entry['eventid'] == "cowrie.session.connect":
-            self.scanip(entry)
+        """
+        events are done in emit() not in write()
+        """
+        pass
 
     @defer.inlineCallbacks
     def crashreport(self, entry):
         """
         Crash report
         """
+        resp = yield treq.post(COWRIE_URL, data={'crash': json.dumps(repr(entry))})
+        content = yield resp.text()
+        if self.debug:
+            print("crashreport: "+content)
