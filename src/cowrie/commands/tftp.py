cc41bef6f28c2de11926a5c9d24c94f23f413052
Michel Oosterhof <michel@oosterhof.net>
2019-09-04 22:27:26 +0100

    no print

--- src/cowrie/commands/tftp.py
+++ src/cowrie/commands/tftp.py
@@ -94,7 +94,7 @@ class command_tftp(HoneyPotCommand):
                 if len(args.c) > 1:
                     self.file_to_get = args.c[1]
                     if args.hostname is None:
-                        self.write("Hostname is invalid")
+                        #self.write("Hostname is invalid")
                         self.exit()
                     self.hostname = args.hostname
 
@@ -106,7 +106,7 @@ class command_tftp(HoneyPotCommand):
                 self.exit()
 
             if self.hostname is None:
-                self.write("Hostname is invalid")
+                #self.write("Hostname is invalid")
                 self.exit()
 
             if self.hostname.find(':') != -1:
