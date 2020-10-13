9473663ab8a69e500c1544afac6135bca873a40e
Peter Šufliarsky <3963179+PeterSufliarsky@users.noreply.github.com>
2020-10-13 21:16:25 +0200

    uniq interactive mode

--- src/cowrie/commands/uniq.py
+++ src/cowrie/commands/uniq.py
@@ -1,7 +1,6 @@
 # Copyright (c) 2020 Peter Sufliarsky <sufliarskyp@gmail.com>
 # See the COPYRIGHT file for more information
 
-
 """
 uniq command
 """
@@ -11,19 +10,25 @@ from __future__ import absolute_import, division
 from twisted.python import log
 from cowrie.shell.command import HoneyPotCommand
 
+
 commands = {}
 
 
 class command_uniq(HoneyPotCommand):
 
+    unique_lines = set()
+
     def start(self):
-        lines = self.input_data.split(b'\n')
-        unique_lines = set(lines)
-        for line in unique_lines:
-            if line:
+        if self.input_data:
+            lines = self.input_data.split(b'\n')
+            if not lines[-1]:
+                lines.pop()
+
+            self.unique_lines = set(lines)
+            for line in self.unique_lines:
                 self.writeBytes(line + b'\n')
 
-        self.exit()
+            self.exit()
 
     def lineReceived(self, line):
         log.msg(eventid='cowrie.command.input',
@@ -31,8 +36,15 @@ class command_uniq(HoneyPotCommand):
                 input=line,
                 format='INPUT (%(realm)s): %(input)s')
 
+        self.grep_input(line)
+
     def handle_CTRL_D(self):
         self.exit()
+
+    def grep_input(self, line):
+        if line not in self.unique_lines:
+            self.writeBytes(line.encode() + b'\n')
+            self.unique_lines.add(line)
 
 
 commands['/usr/bin/uniq'] = command_uniq
