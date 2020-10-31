ed3f079fb3abf102ee7ac492f8807e6f6c601ee9
Peter Šufliarsky <3963179+PeterSufliarsky@users.noreply.github.com>
2020-10-31 20:23:38 +0100

    Use getopt to parse chmod arguments

--- src/cowrie/test/test_chmod.py
+++ src/cowrie/test/test_chmod.py
@@ -20,6 +20,7 @@ os.environ["HONEYPOT_DATA_PATH"] = "../data"
 os.environ["HONEYPOT_DOWNLOAD_PATH"] = "/tmp"
 os.environ["SHELL_FILESYSTEM"] = "../share/cowrie/fs.pickle"
 
+TRY_CHMOD_HELP_MSG = b"Try 'chmod --help' for more information.\n"
 PROMPT = b"root@unitTest:~# "
 
 
@@ -38,67 +39,65 @@ class ShellChmodCommandTests(unittest.TestCase):
         self.proto.lineReceived(b"chmod")
         self.assertEquals(
             self.tr.value(),
-            b"chmod: missing operand\nTry 'chmod --help' for more information.\n"
-            + PROMPT
+            b"chmod: missing operand\n" + TRY_CHMOD_HELP_MSG + PROMPT
         )
 
     def test_chmod_command_002(self):
         """
-        Missing operand after...
+        Missing operand
         """
-        self.proto.lineReceived(b"chmod arg")
+        self.proto.lineReceived(b"chmod -x")
         self.assertEquals(
             self.tr.value(),
-            b"chmod: missing operand after \xe2\x80\x98arg\xe2\x80\x99\nTry 'chmod --help' for more information.\n"
-            + PROMPT
+            b"chmod: missing operand\n" + TRY_CHMOD_HELP_MSG + PROMPT
         )
 
     def test_chmod_command_003(self):
         """
-        Missing operand
+        Missing operand after ...
         """
         self.proto.lineReceived(b"chmod +x")
         self.assertEquals(
             self.tr.value(),
-            b"chmod: missing operand after \xe2\x80\x98+x\xe2\x80\x99\nTry 'chmod --help' for more information.\n"
-            + PROMPT
+            b"chmod: missing operand after \xe2\x80\x98+x\xe2\x80\x99\n" + TRY_CHMOD_HELP_MSG + PROMPT
         )
 
     def test_chmod_command_004(self):
         """
-        No such file or directory
+        Invalid option
         """
-        self.proto.lineReceived(b"chmod +x abcd")
-        self.assertEquals(self.tr.value(), b"chmod: cannot access 'abcd': No such file or directory\n" + PROMPT)
+        self.proto.lineReceived(b"chmod -A")
+        self.assertEquals(
+            self.tr.value(),
+            b"chmod: invalid option -- 'A'\n" + TRY_CHMOD_HELP_MSG + PROMPT
+        )
 
-    # does not work properly
-    # def test_chmod_command_005(self):
-    #     """
-    #     Invalid option
-    #     """
-    #     self.proto.lineReceived(b"chmod -A +x abcd")
-    #     self.assertEquals(
-    #         self.tr.value(),
-    #         b"chmod: invalid option -- 'A'\nTry 'chmod --help' for more information.\n" + PROMPT
-    #     )
+    def test_chmod_command_005(self):
+        """
+        Unrecognized option
+        """
+        self.proto.lineReceived(b"chmod --A")
+        self.assertEquals(
+            self.tr.value(),
+            b"chmod: unrecognized option '--A'\n" + TRY_CHMOD_HELP_MSG + PROMPT
+        )
 
     def test_chmod_command_006(self):
+        """
+        No such file or directory
+        """
+        self.proto.lineReceived(b"chmod -x abcd")
+        self.assertEquals(self.tr.value(), b"chmod: cannot access 'abcd': No such file or directory\n" + PROMPT)
+
+    def test_chmod_command_007(self):
         """
         Invalid mode
         """
         self.proto.lineReceived(b"chmod abcd efgh")
         self.assertEquals(
             self.tr.value(),
-            b"chmod: invalid mode: \xe2\x80\x98abcd\xe2\x80\x99\nTry 'chmod --help' for more information.\n"
-            + PROMPT
+            b"chmod: invalid mode: \xe2\x80\x98abcd\xe2\x80\x99\n" + TRY_CHMOD_HELP_MSG + PROMPT
         )
-
-    def test_chmod_command_007(self):
-        """
-        Valid directory .ssh recursive
-        """
-        self.proto.lineReceived(b"chmod -R +x .ssh")
-        self.assertEquals(self.tr.value(), PROMPT)
 
     def test_chmod_command_008(self):
         """
@@ -109,40 +108,47 @@ class ShellChmodCommandTests(unittest.TestCase):
 
     def test_chmod_command_009(self):
         """
+        Valid directory .ssh recursive
+        """
+        self.proto.lineReceived(b"chmod -R +x .ssh")
+        self.assertEquals(self.tr.value(), PROMPT)
+
+    def test_chmod_command_010(self):
+        """
         Valid directory /root/.ssh
         """
         self.proto.lineReceived(b"chmod +x /root/.ssh")
         self.assertEquals(self.tr.value(), PROMPT)
 
-    def test_chmod_command_010(self):
+    def test_chmod_command_011(self):
         """
         Valid directory ~/.ssh
         """
         self.proto.lineReceived(b"chmod +x ~/.ssh")
         self.assertEquals(self.tr.value(), PROMPT)
 
-    def test_chmod_command_011(self):
+    def test_chmod_command_012(self):
         """
         chmod a+x
         """
         self.proto.lineReceived(b"chmod a+x .ssh")
         self.assertEquals(self.tr.value(), PROMPT)
 
-    def test_chmod_command_012(self):
+    def test_chmod_command_013(self):
         """
         chmod ug+x
         """
         self.proto.lineReceived(b"chmod ug+x .ssh")
         self.assertEquals(self.tr.value(), PROMPT)
 
-    def test_chmod_command_013(self):
+    def test_chmod_command_014(self):
         """
         chmod 777
         """
         self.proto.lineReceived(b"chmod 777 .ssh")
         self.assertEquals(self.tr.value(), PROMPT)
 
-    def test_chmod_command_014(self):
+    def test_chmod_command_015(self):
         """
         chmod 0775
         """
