c10ae32089ccc54389072d8eea56d0ebc51574f1
Julius ter Pelkwijk <pelkwijk@gmail.com>
2020-11-10 11:55:42 +0100

    Edited help to get rid of code smell

--- src/cowrie/commands/unzip.py
+++ src/cowrie/commands/unzip.py
@@ -27,32 +27,30 @@ class command_unzip(HoneyPotCommand):
 
     def call(self):
         if len(self.args) == 0 or self.args[0].startswith('-'):
-            self.write(
-            """UnZip 6.00 of 20 April 2009, by Debian. Original by Info-ZIP.
-
-Usage: unzip [-Z] [-opts[modifiers]] file[.zip] [list] [-x xlist] [-d exdir]
-  Default action is to extract files in list, except those in xlist, to exdir;
-  file[.zip] may be a wildcard.  -Z => ZipInfo mode ("unzip -Z" for usage).
-
-  -p  extract files to pipe, no messages     -l  list files (short format)
-  -f  freshen existing files, create none    -t  test compressed archive data
-  -u  update files, create if necessary      -z  display archive comment only
-  -v  list verbosely/show version info       -T  timestamp archive to latest
-  -x  exclude files that follow (in xlist)   -d  extract files into exdir
-modifiers:
-  -n  never overwrite existing files         -q  quiet mode (-qq => quieter)
-  -o  overwrite files WITHOUT prompting      -a  auto-convert any text files
-  -j  junk paths (do not make directories)   -aa treat ALL files as text
-  -U  use escapes for all non-ASCII Unicode  -UU ignore any Unicode fields
-  -C  match filenames case-insensitively     -L  make (some) names lowercase
-  -X  restore UID/GID info                   -V  retain VMS version numbers
-  -K  keep setuid/setgid/tacky permissions   -M  pipe through "more" pager
-See "unzip -hh" or unzip.txt for more help.  Examples:
-  unzip data1 -x joe   => extract all files except joe from zipfile data1.zip
-  unzip -p foo | more  => send contents of foo.zip via pipe into program more
-  unzip -fo foo ReadMe => quietly replace existing ReadMe if archive file newer
-"""
-            )
+            output = ('UnZip 6.00 of 20 April 2009, by Debian. Original by Info-ZIP.\n'
+            '\n'
+            'Usage: unzip [-Z] [-opts[modifiers]] file[.zip] [list] [-x xlist] [-d exdir]\n'
+            '  Default action is to extract files in list, except those in xlist, to exdir;\n'
+            '  file[.zip] may be a wildcard.  -Z => ZipInfo mode ("unzip -Z" for usage).\n'
+            '\n'
+            '  -p  extract files to pipe, no messages     -l  list files (short format)\n'
+            '  -f  freshen existing files, create none    -t  test compressed archive data\n'
+            '  -u  update files, create if necessary      -z  display archive comment only\n'
+            '  -v  list verbosely/show version info       -T  timestamp archive to latest\n'
+            '  -x  exclude files that follow (in xlist)   -d  extract files into exdir\n'
+            'modifiers:\n'
+            '   -n  never overwrite existing files         -q  quiet mode (-qq => quieter)\n'
+            '  -o  overwrite files WITHOUT prompting      -a  auto-convert any text files\n'
+            '  -j  junk paths (do not make directories)   -aa treat ALL files as text\n'
+            '  -U  use escapes for all non-ASCII Unicode  -UU ignore any Unicode fields\n'
+            '  -C  match filenames case-insensitively     -L  make (some) names lowercase\n'
+            '  -X  restore UID/GID info                   -V  retain VMS version numbers\n'
+            '  -K  keep setuid/setgid/tacky permissions   -M  pipe through "more" pager\n'
+            'See "unzip -hh" or unzip.txt for more help.  Examples:\n'
+            '  unzip data1 -x joe   => extract all files except joe from zipfile data1.zip\n'
+            '  unzip -p foo | more  => send contents of foo.zip via pipe into program more\n'
+            '  unzip -fo foo ReadMe => quietly replace existing ReadMe if archive file newer\n')
+            self.write(output)
             return
 
         filename = self.args[0]
@@ -70,24 +68,22 @@ See "unzip -hh" or unzip.txt for more help.  Examples:
 
         f = self.fs.getfile(path)
         if not f[A_REALFILE]:
-            self.write(""" End-of-central-directory signature not found.  Either this file is not
-  a zipfile, or it constitutes one disk of a multi-part archive.  In the
-  latter case the central directory and zipfile comment will be found on
-  the last disk(s) of this archive.
-"""
-            )
+            output = ('  End-of-central-directory signature not found.  Either this file is not\n'
+            '  a zipfile, or it constitutes one disk of a multi-part archive.  In the\n'
+            '  latter case the central directory and zipfile comment will be found on\n'
+            '  the last disk(s) of this archive.\n')
+            self.write(output)
             self.write('unzip:  cannot find or open {0}, {0}.zip or {0}.ZIP.\n'.format(filename))
             return
 
         try:
             t = zipfile.ZipFile(f[A_REALFILE]).infolist()
         except Exception:
-            self.write(""" End-of-central-directory signature not found.  Either this file is not
-  a zipfile, or it constitutes one disk of a multi-part archive.  In the
-  latter case the central directory and zipfile comment will be found on
-  the last disk(s) of this archive.
-"""
-            )
+            output = ('  End-of-central-directory signature not found.  Either this file is not\n'
+            '  a zipfile, or it constitutes one disk of a multi-part archive.  In the\n'
+            '  latter case the central directory and zipfile comment will be found on\n'
+            '  the last disk(s) of this archive.\n')
+            self.write(output)
             self.write('unzip:   cannot find zipfile directory in one of {0}, {0}.zip or {0}.ZIP.\n'.format(filename))
             return
         self.write('Archive:  {}\n'.format(filename))
