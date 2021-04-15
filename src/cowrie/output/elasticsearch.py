5200ee56a7c5f085670d6b257ef516aec664e7ff
Michel Oosterhof <michel@oosterhof.net>
2021-04-15 20:04:07 +0800

    typing

--- src/cowrie/output/elasticsearch.py
+++ src/cowrie/output/elasticsearch.py
@@ -13,44 +13,47 @@ class Output(cowrie.core.output.Output):
     """
     elasticsearch output
     """
+    index: str
+    es: Elasticsearch
+    pipeline: str
 
     def start(self):
-        self.host = CowrieConfig.get("output_elasticsearch", "host")
-        self.port = CowrieConfig.get("output_elasticsearch", "port")
+        host = CowrieConfig.get("output_elasticsearch", "host")
+        port = CowrieConfig.get("output_elasticsearch", "port")
         self.index = CowrieConfig.get("output_elasticsearch", "index")
         self.type = CowrieConfig.get("output_elasticsearch", "type")
         self.pipeline = CowrieConfig.get("output_elasticsearch", "pipeline")
         # new options (creds + https)
-        self.username = CowrieConfig.get(
+        username = CowrieConfig.get(
             "output_elasticsearch", "username", fallback=None
         )
-        self.password = CowrieConfig.get(
+        password = CowrieConfig.get(
             "output_elasticsearch", "password", fallback=None
         )
-        self.use_ssl = CowrieConfig.getboolean(
+        use_ssl = CowrieConfig.getboolean(
             "output_elasticsearch", "ssl", fallback=False
         )
-        self.ca_certs = CowrieConfig.get(
+        ca_certs = CowrieConfig.get(
             "output_elasticsearch", "ca_certs", fallback=None
         )
-        self.verify_certs = CowrieConfig.getboolean(
+        verify_certs = CowrieConfig.getboolean(
             "output_elasticsearch", "verify_certs", fallback=True
         )
 
         options: Dict[str, Any] = {}
         # connect
-        if (self.username is not None) and (self.password is not None):
-            options["http_auth"] = (self.username, self.password)
-        if self.use_ssl:
+        if (username is not None) and (password is not None):
+            options["http_auth"] = (username, password)
+        if use_ssl:
             options["scheme"] = "https"
-            options["use_ssl"] = self.use_ssl
+            options["use_ssl"] = use_ssl
             options["ssl_show_warn"] = False
-            options["verify_certs"] = self.verify_certs
-            if self.verify_certs:
-                options["ca_certs"] = self.ca_certs
+            options["verify_certs"] = verify_certs
+            if verify_certs:
+                options["ca_certs"] = ca_certs
 
         # connect
-        self.es = Elasticsearch(f"{self.host}:{self.port}", **options)
+        self.es = Elasticsearch(f"{host}:{port}", **options)
         # self.es = Elasticsearch('{0}:{1}'.format(self.host, self.port))
 
         self.check_index()
