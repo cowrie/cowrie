4bf108fa8de6a4182eb84e79b3c586785182859d
Michel Oosterhof <michel@oosterhof.net>
2021-05-07 14:04:26 +0800

    more typing

--- src/cowrie/shell/command.py
+++ src/cowrie/shell/command.py
@@ -33,7 +33,9 @@ class HoneyPotCommand:
         self.environ = self.protocol.cmdstack[0].environ
         self.fs = self.protocol.fs
         self.data: bytes = None  # output data
-        self.input_data: Optional[bytes] = None  # used to store STDIN data passed via PIPE
+        self.input_data: Optional[
+            bytes
+        ] = None  # used to store STDIN data passed via PIPE
         self.writefn: Callable[[bytes], None] = self.protocol.pp.outReceived
         self.errorWritefn: Callable[[bytes], None] = self.protocol.pp.errReceived
         # MS-DOS style redirect handling, inside the command
