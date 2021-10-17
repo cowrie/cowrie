bb0460d75a50c651e1acd48c3fa1dc3d4a2fac70
Michel Oosterhof <michel@oosterhof.net>
2021-10-17 19:07:44 +0800

    moarr!

--- src/cowrie/commands/adduser.py
+++ src/cowrie/commands/adduser.py
@@ -3,6 +3,8 @@
 
 from __future__ import annotations
 
+from typing import Any, List, Tuple
+
 import random
 from typing import Optional
 
@@ -16,20 +18,8 @@ O_O, O_Q, O_P = 1, 2, 3
 
 
 class Command_adduser(HoneyPotCommand):
-    def start(self):
-        self.username: Optional[str] = None
-        self.item: int = 0
-        for arg in self.args:
-            if arg.startswith("-") or arg.isdigit():
-                continue
-            self.username = arg
-            break
-        if self.username is None:
-            self.write("adduser: Only one or two names allowed.\n")
-            self.exit()
-            return
-
-        self.output = [
+    item: int
+    output: List[Tuple[int, str]] = [
             (O_O, "Adding user `%(username)s' ...\n"),
             (O_O, "Adding new group `%(username)s' (1001) ...\n"),
             (
@@ -60,6 +50,20 @@ class Command_adduser(HoneyPotCommand):
             (O_O, "Deleting home directory `/home/%(username)s' ...\n"),
             (O_Q, "Try again? [Y/n] "),
         ]
+    username: Optional[str] = None
+
+    def start(self):
+        self.item = 0
+        for arg in self.args:
+            if arg.startswith("-") or arg.isdigit():
+                continue
+            self.username = arg
+            break
+        if self.username is None:
+            self.write("adduser: Only one or two names allowed.\n")
+            self.exit()
+            return
+
         self.do_output()
 
     def do_output(self):
