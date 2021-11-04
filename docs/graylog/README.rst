3d43ed66073ef3dacc5c83facbd78a15fe50602f
Sergey Malinkin <malinkinsa@yandex.ru>
2021-11-04 21:41:06 +0300

    Add output to Graylog via GELF HTTP input.

--- docs/graylog/README.rst
+++ docs/graylog/README.rst
@@ -1,4 +1,4 @@
-How to send Cowrie output to Graylog
+How to send Cowrie output to Graylog via syslog
 ####################################
 
 
@@ -46,4 +46,43 @@ Restart rsyslog::
 
     $ sudo service rsyslog restart
 
+How to send Cowrie output to Graylog via HTTP GELF input
+####################################
 
+
+Prerequisites
+======================
+
+* Working Cowrie installation
+* Working Graylog installation
+* Working HTTP Gelf Input on Graylog
+
+Cowrie Configuration
+======================
+
+Open the Cowrie configuration file and find this block ::
+
+    [output_graylog_gelf]
+    enabled = false
+    url = http://127.0.0.1:12201/gelf
+    tls = False
+    cert =
+    key =
+
+Enable this block and specify url of your input.
+If you need tls - change status to True and specify path to your tls cert and key.
+
+Restart Cowrie
+
+Graylog Configuration
+======================
+
+Open the Graylog web interface and click on the **System** drop-down in the top menu. From the drop-down menu select **Inputs**. Select **GELF HTTP** from the drop-down menu and click the **Launch new input** button. In the modal dialog enter the information about your input.
+
+Then click **Launch.**
+
+Note:
+
+- If TLS is enabled - in URL block you must specify **https://**
+
+- Do not remove **/gelf** from the end of URL block, expect of case when your proxing this address behind nginx;
---
+++ src/cowrie/output/graylog.py
@@ -0,0 +1,60 @@
+"""
+Simple Graylog HTTP Graylog Extended Log Format (GELF) logger.
+"""
+
+import cowrie.core.output
+import json
+import os
+import requests
+import time
+
+from cowrie.core.config import CowrieConfig
+
+
+url: bytes
+
+
+class Output(cowrie.core.output.Output):
+    def start(self):
+        self.url = CowrieConfig.get("output_graylog_gelf", "url")
+        self.tls = CowrieConfig.get("output_graylog_gelf", "tls")
+        if 'True' in self.tls:
+            self.cert = CowrieConfig.get("output_graylog_gelf", "cert")
+            self.cert_key = CowrieConfig.get("output_graylog_gelf", "key")
+
+        self.headers = {
+            'Content-Type': 'application/json'
+        }
+        self.hostname = os.uname()[1]
+
+    def stop(self):
+        pass
+
+    def write(self, logentry):
+        for i in list(logentry.keys()):
+            # Remove twisted 15 legacy keys
+            if i.startswith("log_"):
+                del logentry[i]
+
+        self.gelf_message = {
+            'version': '1.1',
+            'host': self.hostname,
+            'timestamp': time.time(),
+            'short_message': json.dumps(logentry),
+            'level': 1,
+        }
+
+        if 'False' in self.tls:
+            self.gelf = requests.post(
+                self.url,
+                headers=self.headers,
+                data=json.dumps(self.gelf_message)
+            )
+        else:
+            self.gelf = requests.post(
+                self.url,
+                verify=False,
+                cert=(self.cert, self.cert_key),
+                headers=self.headers,
+                data=json.dumps(self.gelf_message)
+            )
