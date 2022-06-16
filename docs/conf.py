080f5dbe0dd0c945f3fadab62152faf3ed9126c7
Michel Oosterhof <michel@oosterhof.net>
2022-06-16 13:21:45 +0800

    set language for recent sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ from cowrie import version as cowrie_version_object
 # -- Project information -----------------------------------------------------
 
 project = "cowrie"
-copyright = "2014-2021, Michel Oosterhof"
+copyright = "2014-2022, Michel Oosterhof"
 author = "Michel Oosterhof"
 
 # The version info for the project you're documenting, acts as replacement for
@@ -70,7 +70,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
