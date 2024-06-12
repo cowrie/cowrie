2bf82eb5852970f0a210d6a712550747fa512b50
Oscar Palmqvist <oscar@palmqvist.eu>
2024-06-12 13:11:12 +0200

    random times and sizes

--- src/model/cowrie_handler.py
+++ src/model/cowrie_handler.py
@@ -1,4 +1,5 @@
-import os
+import random
+import time
 
 class CowrieHandler():
     def __init__(self, fs) -> None:
@@ -10,14 +11,20 @@ class CowrieHandler():
         def is_file(item: str):
             return "." in item
         
+        def random_time(months_ago):
+            ctime = time.time()
+            return ctime-random.uniform(0, months_ago*30*24*60*60)
+
+        def random_size():
+            return random.randrange(1024, 4000000, 1024)
+        
         for item in items:
             item_path = path+"/"+item
             if not self.fs.exists(item_path):
                 if is_file(item):
-                    with open(item_path, "w") as file:
-                        pass
+                    self.fs.mkfile(item_path, 0, 0, random_size(), 33188, random_time(6))
                 else:
-                    self.fs.mkdir(item_path, 0, 0, 4096, 33188)
+                    self.fs.mkdir(item_path, 0, 0, 4096, 33188, random_time(6))
 
 
 
