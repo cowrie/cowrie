a882501aeff91bff5fdc789aa7d75107093f502a
emsjob <emilsjob@kth.se>
2024-06-14 14:44:37 +0200

    token to download model

--- src/model/download_models.py
+++ src/model/download_models.py
@@ -4,13 +4,14 @@ import os
 def download_model(model_path, model_name):
     """Download a Hugging Face model and tokenizer to the specified directory"""
     # Check if the directory already exists
+    token = open("token.txt", "r").read()
     if not os.path.exists(model_path):
         # Create the directory
         os.umask(0)
         os.makedirs(model_path, mode=0o777)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
+    model = AutoModelForCausalLM.from_pretrained(model_name, token=token, device_map="auto")
 
     # Save the model and tokenizer to the specified directory
     model.save_pretrained(model_path)
