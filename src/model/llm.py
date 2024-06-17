48a925ead0b5bf208a9cbc4892cb97a0496a9979
emsjob <emilsjob@kth.se>
2024-06-17 12:01:12 +0200

    Adding command lookup table

--- src/model/llm.py
+++ src/model/llm.py
@@ -1,4 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import json
+
+with open("command_lookup.json", "r") as f:
+    LOOKUPS = json.load(f)
 
 class LLM:
     def __init__(self, model_name="google/codegemma-7b-it"):
@@ -6,8 +10,20 @@ class LLM:
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
         self.model = AutoModelForCausalLM.from_pretrained(model_name, token=token, device_map="auto")
 
+    def get_example_message(self, cmd):
+        return command_lookup[cmd]
+
+    def create_messages(self, base_prompt, cmd):
+        answer = LOOKUPS[cmd]
+        messages = [
+            {"role": "user", "content": base_prompt},
+            {"role": "assistant", "content": answer},
+            {"role": "user", "content": cmd}
+        ]
+        return messages
+
     def generate_response(self, cmd):
-        base_prompt = "You are Linux OS terminal for a server containing sensitive patient data. "+\
+        base_prompt = f"You are Linux OS terminal for a server containing sensitive patient data. "+\
             "Your personality is: You are a Linux OS terminal. You act and respond exactly as a Linux terminal. "+\
             "You will respond to all commands just as a Linux terminal would. " +\
             "You can only respond to user inputs and you must not write any commands on your own. " +\
@@ -15,13 +31,15 @@ class LLM:
             "Do not explain to user what they are seeing. Only respond as Linux terminal. "+\
             "You will need to make up realistic answers to the command, as they would be returned by a real linux terminal for a hospital server. "+\
             "It is very important that you do not name files and directiories file1.txt file2.txt file3.txt or similarly, rather create plausible file names for a real terminal with patient data.\n\n"+\
-            "pwd"
-        answer1 = "/home/admin"
-        prompt2 = cmd 
-        messages = [{"role": "user", "content": base_prompt},
-                    {"role": "assistant", "content": answer1},
-                    {"role": "user", "content": prompt2}]
+            "{cmd}"
 
+        #answer1 = 
+        #prompt2 = cmd
+        #messages = [{"role": "user", "content": base_prompt},
+        #            {"role": "assistant", "content": answer1},
+        #            {"role": "user", "content": prompt2}]
+
+        messages = self.create_messages(base_prompt, cmd)
         tokenized_chat = self.tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
         len_chat = tokenized_chat.shape[1]
         outputs = self.model.generate(tokenized_chat, max_new_tokens=100)
