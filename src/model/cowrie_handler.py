07f48760d86beec2ad5fd4922b43949a72595d41
Oscar Palmqvist <oscar@palmqvist.eu>
2024-06-25 12:49:54 +0200

    files are now real when they need to be

--- src/model/cowrie_handler.py
+++ src/model/cowrie_handler.py
@@ -1,9 +1,13 @@
 import random
 import time
+import os
+import re
+import stat
 
 class CowrieHandler():
-    def __init__(self, fs) -> None:
-        self.fs = fs
+    def __init__(self, protocol) -> None:
+        self.protocol = protocol
+        self.fs = protocol.fs
     
     def enforce_ls(self, path: str, ls_view: str):
         items = ls_view.split(" ") 
@@ -18,13 +22,32 @@ class CowrieHandler():
         def random_size():
             return random.randrange(1024, int(4e6), 1024)
         
+        perm = stat.S_IFREG |stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
         for item in items:
             item_path = path+"/"+item
             if not self.fs.exists(item_path):
                 if is_file(item):
-                    self.fs.mkfile(item_path, 0, 0, random_size(), 33188, random_time(6), is_llm=True)
+                    self.fs.mkfile(item_path, 0, 0, random_size(), perm, random_time(6), is_llm=True)
                 else:
-                    self.fs.mkdir(item_path, 0, 0, 4096, 33188, random_time(6), is_llm=True)
+                    self.fs.mkdir(item_path, 0, 0, 4096, perm, random_time(6), is_llm=True)
+
+    def enforce_file_contents(self, path, contents):
+        tmp_fname = "{}-{}-{}-redir_{}".format(
+            time.strftime("%Y%m%d-%H%M%S"),
+            self.protocol.getProtoTransport().transportId,
+            self.protocol.terminal.transport.session.id,
+            re.sub("[^A-Za-z0-9]", "_", path),
+        )
+        #TODO: Change this to get from CowrieConfig
+        safeoutfile = "var/lib/cowrie/downloads/"+tmp_fname
+
+        data = bytes(contents, encoding='utf-8')
+        with open(safeoutfile, "ab") as f:
+            f.write(data)
+
+        fs_f = self.fs.getfile(path)
+        self.fs.update_realfile(fs_f, safeoutfile)
+        self.fs.update_file_size(fs_f, len(data))
 
 
 
