b09a33ca5916656d2342b5a25ce03d81767e4768
emsjob <emilsjob@kth.se>
2024-07-08 10:57:37 -0500

    adding token to model download

--- src/model/download_models.py
+++ src/model/download_models.py
@@ -7,10 +7,10 @@ RESPONSE_PATH = "/cowrie/cowrie-git/src/model"
 
 def download_model(model_path, model_name):
     """Download a Hugging Face model and tokenizer to the specified directory"""
-    '''
     with open(f"{RESPONSE_PATH}/token.txt", "r") as f:
         token = f.read().rstrip()
-
+    
+    '''
     if not os.path.exists(model_path):
         os.umask(0)
         os.makedirs(model_path, mode=0o777)
@@ -22,7 +22,7 @@ def download_model(model_path, model_name):
     tokenizer.save_pretrained(model_path+model_name)
     '''
 
-    snapshot_download(repo_id=model_name)
+    snapshot_download(repo_id=model_name, token=token)
 
 if os.environ["COWRIE_USE_LLM"].lower() == "true":
     download_model("models/", "google/codegemma-7b-it")
