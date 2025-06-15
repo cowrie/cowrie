380487f76acddba491d1b9c9215fa7ceadbc6be4
Michel Oosterhof <michel@oosterhof.net>
2025-06-16 00:01:50 +0200

    remove protocol

--- src/cowrie/ssh_proxy/client_transport.py
+++ src/cowrie/ssh_proxy/client_transport.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 from typing import Any
 
 from twisted.conch.ssh import factory, transport
-from twisted.internet import defer, protocol
+from twisted.internet import defer
 from twisted.protocols.policies import TimeoutMixin
 from twisted.python import log
 
