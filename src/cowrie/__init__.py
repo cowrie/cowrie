288f7e79c577128228aeac16c804ed04eaac0ac3
Michel Oosterhof <michel@oosterhof.net>
2025-09-19 20:44:15 +0800

    up

--- src/cowrie/__init__.py
+++ src/cowrie/__init__.py
@@ -1,1 +1,8 @@
-import cowrie._version as __version__
+import sys
+from twisted.python import log
+
+try:
+    import cowrie._version as __version__  # noqa: F401
+except ModuleNotFoundError:
+    log.err("Cowrie is not installed. Run `pip install -e .` to install Cowrie into your virtual enviroment")
+    sys.exit(1)
